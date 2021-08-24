34da917bfc640cada48e477665efa956e3630b31
Jon Yurek <jyurek@gmail.com>
2021-08-24 08:44:14 -0400

    Generate the Devise User model
    We ran `rails g devise User` and then modified the migration so that the
    existing `users` table is modified to use devise conventions.
    
    The devise model configuration is:
    
    * confirmable: Sends an email confirmation to the user on signup. Can be
      reconfirmed when the user's email changes.
    * database_authenticatable: Allows the user to be authenticated via encryted
      passwords stored in the database.
    * registerable: Users can create new accounts on their own via a signup form.
    * recoverable: Provides a "forgot password" set o functionality that allows
      the user to set a new password.
    * rememberable: Adds the ability for the site to "Remember Me" and not have to
      log in each time they open the browser.
    * validatable: Adds validations to the User model automatically. Emails must
      be in a generally email-address format and passwords must be between 8 and
      128 characters.

---
+++ db/migrate/20210824110701_devise_create_users.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :users, :EmailAddress, :email
+    rename_column :users, :password_digest, :encrypted_password
+    rename_column :users, :reset_digest, :reset_password_token
+    rename_column :users, :reset_sent_at, :reset_password_sent_at
+    add_column :users, :remember_created_at, :datetime
+    rename_column :users, :activation_digest, :confirmation_token
+    add_column :users, :confirmed_at, :datetime
+    reversible do |dir|
+      dir.up do
+        execute("UPDATE users SET confirmed_at = NOW() WHERE activated = 1")
+      end
+      dir.down do
+        execute("UPDATE users SET activated = (confirmed_at IS NOT NULL)")
+      end
+    end
+    remove_column :users, :activated, :boolean, default: false
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+
+    change_column_null :users, :email, false
+    change_column_null :users, :encrypted_password, false
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :confirmation_token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_200245) do
+ActiveRecord::Schema.define(version: 2021_08_24_110701) do
 
   create_table "ARPart5Publications", primary_key: "EndNoteID", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "ReserveID"
@@ -873,7 +873,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_200245) do
     t.string "PermAddrPostalCode", limit: 20
     t.integer "PermAddrStateID"
     t.integer "PermAddrCountryID"
-    t.string "EmailAddress", limit: 100
+    t.string "email", limit: 100, null: false
     t.string "CellPhone", limit: 20
     t.string "FaxPhone", limit: 20
     t.string "OtherPhone", limit: 20
@@ -900,19 +900,25 @@ ActiveRecord::Schema.define(version: 2021_08_23_200245) do
     t.string "Advisor", limit: 100, comment: "Advisor or Supervisor"
     t.string "ORCID", limit: 50, comment: "Unique ID for Researchers https://orcid.org/"
     t.datetime "DateCreated", default: -> { "CURRENT_TIMESTAMP" }, null: false, comment: "Use to determain if need to update record"
-    t.string "activation_digest", limit: 100
-    t.string "reset_digest", limit: 100
-    t.datetime "reset_sent_at"
+    t.string "confirmation_token", limit: 100
+    t.string "reset_password_token", limit: 100
+    t.datetime "reset_password_sent_at"
     t.boolean "admin", default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "password_digest"
-    t.boolean "activated", default: false
+    t.string "encrypted_password", null: false
+    t.datetime "remember_created_at"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["InstitutionID", "NameLast", "NameFirst", "NameMiddle"], name: "Institution+Name"
     t.index ["InstitutionID"], name: "Institution"
     t.index ["NameGroup", "NameLast", "NameFirst"], name: "Group"
     t.index ["NameLast", "NameFirst", "NameMiddle"], name: "Name"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["id"], name: "user"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
