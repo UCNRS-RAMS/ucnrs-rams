7988383492a23e736401abb409c825a4a89770ca
Jon Yurek <jyurek@gmail.com>
2021-08-25 10:32:50 -0400

    Configure SMTP in Production Env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,23 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Default Mailer Host
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch("HOST"),
+    protocol: ENV.fetch("PROTOCOL", "https")
+  }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch("SMTP_HOST"),
+    port: ENV.fetch("SMTP_PORT"),
+    domain: ENV.fetch("SMTP_DOMAIN"),
+    user_name: ENV.fetch("SMTP_USERNAME"),
+    password: ENV.fetch("SMTP_PASSWORD"),
+    authentication: :login,
+    enable_starttls_auto: true  
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
