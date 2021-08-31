3e3bffea451c95e30cb8549794b1f6d585143cbd
Jon Yurek <jyurek@gmail.com>
2021-08-31 15:02:39 -0400

    Styled CSS for Forgot Password page
    Since the Unauthenticated views were unstyled this page contains the styled
    layout for those views as well as a sample view, which is the Forgot Password
    view.
    
    This commit also changes the Forgot Password view into a normal view instead
    of a Component. The Components are for resuable, dynamic, testable classes
    that represent view functionality. This is not really what that view needed,
    and so became a little unnecessarily complicated. So we backed it out and it's
    simpler now.

--- app/controllers/unauthenticated/passwords_controller.rb
+++ app/controllers/unauthenticated/passwords_controller.rb
@@ -1,9 +1,5 @@
 module Unauthenticated
   class PasswordsController < Devise::PasswordsController
     layout "unauthenticated"
-
-    def index
-      render(ForgotPasswordComponent.new(current_user))
-    end
   end
 end
