3a758112f8c814bf29cd97fb541c733b68705023
Ethan Fertsch <efertsch@gmail.com>
2021-09-01 15:19:25 -0400

    Update institutions table and add model
    This commit renames the `institutions` table and its columns in order to bring them to parity with rails naming conventions. It makes several other modifications to the database as well including changing column names, adding null constraints to ids across several tables, renaming some indicies, and renaming columns in other tables that reference what was previously `InstitutionID`.
    
    It also adds the model, with a subset of validations and associations (the rest of which will be added soon as other models are built) and tests and updates the `Country` and `State` models' associations and validations to include `institutions`.

---
+++ app/models/institution.rb
@@ -0,0 +1,25 @@
+class Institution < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :city, case_sensitive: false }
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :state, presence: true
+  validates :institution_type, presence: true
+
+  belongs_to :state
+  belongs_to :country
+
+  enum institution_type: {
+    university_of_california: "University of California",
+    california_state_university_system: "California State University System",
+    california_community_college: "California Community College",
+    other_california_university_or_college: "California - Other University or College",
+    non_california_us_university_or_college: "U.S. - University or College Outside of California",
+    international_university_or_college: "International University or College",
+    k_12_education: "K-12 Education",
+    non_governmental_organization_or_entity: "Non-Governmental Organization or Non-Profit Entity",
+    governmental_organization_or_entity: "Governmental Agency or Entity",
+    business_entity: "Business Entity",
+    individual_or_other_entity: "Individual or Other Entity",
+  }
+end
