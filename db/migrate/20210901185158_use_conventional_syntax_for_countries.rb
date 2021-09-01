b30af1c0caeab99d2e25929daf3fb148cfafa390
Ethan Fertsch <efertsch@gmail.com>
2021-09-01 14:58:11 -0400

    Update countries table and add model
    This commit renames the `countries` table and its columns in order to bring them to parity with rails naming conventions. It makes several other modifications to the database as well including adding a null constraint to an id that corresponds to a country in a different table, renaming some indicies, and renaming columns in other tables that reference what was previously `CountryID`.
    
    It also adds the model, with a subset of validations and associations (the rest of which will be added soon as other models are built) and tests.

---
+++ db/migrate/20210901185158_use_conventional_syntax_for_countries.rb
@@ -0,0 +1,20 @@
+class UseConventionalSyntaxForCountries < ActiveRecord::Migration[6.1]
+  def change
+    rename_table :Countries, :countries_
+    rename_table :countries_, :countries
+    rename_column :countries, :Name, :name
+    rename_column :countries, :Code, :code
+    rename_column :countries, :Subunit, :subunit
+
+    rename_column :countries, :CountryID, :id
+    rename_column :Institutions, :CountryID, :country_id
+    rename_column :States, :CountryID, :country_id
+    rename_column :users, :AddrCountryID, :address_country_id
+    rename_column :users, :BillingAddrCountryID, :billing_address_country_id
+
+    change_column_null :users, :address_country_id, false
+
+    rename_index :countries, :Name, :name
+    rename_index :States, :Country, :country
+  end
+end
