5107a59bc71d8bf4772a266c8d2d06678c5bc3dd
Ethan Fertsch <efertsch@gmail.com>
2021-09-01 14:58:11 -0400

    Update countries table and add model
    This commit renames the `countries` table and its columns in order to bring them to parity with rails naming conventions. It makes several other modifications to the database as well including adding a null constraint to an id that corresponds to a country in a different table, renaming some indicies, and renaming columns in other tables that reference what was previously `CountryID`.
    
    It also adds the model, with a subset of validations and associations (the rest of which will be added soon as other models are built) and tests.

---
+++ app/models/country.rb
@@ -0,0 +1,6 @@
+class Country < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :user_address_countries, class_name: "User", foreign_key: :address_country_id
+  has_many :user_billing_address_countries, class_name: "User", foreign_key: :billing_address_country_id
+end
