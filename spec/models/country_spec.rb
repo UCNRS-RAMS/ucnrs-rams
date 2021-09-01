385b522159566eb411bff61389eebe8fe4ed1abd
Ethan Fertsch <efertsch@gmail.com>
2021-09-01 13:39:50 -0400

    country model validations

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Country, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
   describe "associations" do
     it { is_expected.to have_many(:institutions) }
     it { is_expected.to have_many(:user_address_countries).class_name("User").with_foreign_key(:address_country_id) }
