ab39b6ebf83b998215bee289db987e52f0d5396d
Jon Yurek <jyurek@gmail.com>
2021-09-14 09:23:22 -0400

    Add Accesssibility Concenrs to the Welcome Modal
    Modals can still interact well with screenreaders. Following this guide:
    http://web-accessibility.carnegiemuseums.org/code/dialogs/, this commit
    attempts to make the modals more accessible. The aria-hidden attribute is
    employed to make sure the text of the page is not going to be read when it's
    not relevant, and also to make sure that the modal text is not read when it's
    not visible. Also, it adds a non-visible description as to what the modal is
    for.

--- app/javascript/controllers/modal_controller.ts
+++ app/javascript/controllers/modal_controller.ts
@@ -7,9 +7,13 @@ export default class extends Controller {
 
   open() {
     this.element.classList.add("visible")
+    this.element.setAttribute("aria-hidden", "false")
+    document.getElementsByTagName("main")[0].setAttribute("aria-hidden", "true")
   }
 
   close() {
     this.element.classList.remove("visible")
+    this.element.setAttribute("aria-hidden", "true")
+    document.getElementsByTagName("main")[0].setAttribute("aria-hidden", "false")
   }
 }
