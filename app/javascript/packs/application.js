ab8de669e6602488091b359072cdd09647d38d87
Ethan Fertsch <efertsch@gmail.com>
2021-10-19 15:42:21 -0400

    Add turbo
    This commit removes `turbolinks` and adds `turbo` via the `turbo-rails` gem. The changes to the `package.json`, `yarn.lock`, and `config/cable.yml` files are the result of running `./bin/rails turbo:install`.
    
    The JavaScript entry point file was modified to `import { Turbo } from "@hotwired/turbo-rails"` per the instructions generated by the installer.
    
    Note: I did not run `./bin/rails turbo:install:redis`, which changes the development Action Cable adapter from Async (the default one) to Redis, but it looks like the changes may have occured anyway when the installer was run?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { Turbo } from "@hotwired/turbo-rails"
 
 Rails.start()
 ActiveStorage.start()
