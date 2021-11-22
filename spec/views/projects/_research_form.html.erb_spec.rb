7a5522c6a1fda8d6890311377efd16bbae5ffe9c
Ethan Fertsch <efertsch@gmail.com>
2021-11-22 14:43:52 -0500

    Add markup for discipline section in project form
    This commit adds the markup that is required to select a discipline for a project. The disciplines are hardcoded on the Project model and are each represented by a radio button in the view. There is also a text field to collect a plain text entry when the 'Other' discipline is selected.
    
    The validations for the discipline and discipline_other fields were also added as part of this work.

--- spec/views/projects/_research_form.html.erb_spec.rb
+++ spec/views/projects/_research_form.html.erb_spec.rb
@@ -17,4 +17,31 @@ RSpec.describe "app/views/projects/_research_form.html.erb", type: :view do
     expect(doc).to have_field("Thesis Title", type: "text")
     expect(doc).to have_field("Project Abstract", type: "textarea")
   end
+
+  it "has the required fields in the 'Disciplines' section" do
+    presenter = ProjectsNewPresenter.new(
+      user: :fake_user,
+      current_step: 1,
+    )
+
+    FakeForm.fields_for(ProjectForm.new) do |form|
+      render partial: "projects/research_form",
+        locals: { presenter: presenter, form: form }
+    end
+
+    doc = Capybara.string(rendered)
+    expect(doc).to have_field("Agriculture", type: "radio")
+    expect(doc).to have_field("Arts/Humanities", type: "radio")
+    expect(doc).to have_field("Medical, Health & Safety", type: "radio")
+    expect(doc).to have_field("Biology", type: "radio")
+    expect(doc).to have_field("Earth Sciences", type: "radio")
+    expect(doc).to have_field("Education", type: "radio")
+    expect(doc).to have_field("Engineering/Computer Science", type: "radio")
+    expect(doc).to have_field("Environmental Science/Natural Resources", type: "radio")
+    expect(doc).to have_field("Physical Sciences", type: "radio")
+    expect(doc).to have_field("Social Sciences", type: "radio")
+    expect(doc).to have_field("Veterinary Medicine", type: "radio")
+    expect(doc).to have_field("Other", type: "radio")
+    expect(doc).to have_field("project_discipline_other", type: "text")
+  end
 end
