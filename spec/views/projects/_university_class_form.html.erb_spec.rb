ef390382dc3f6cab4eceabd72e8de8fb3ecce44f
Ethan Fertsch <efertsch@gmail.com>
2021-11-26 11:47:09 -0500

    Add markup for class project details section
    This commit adds the markup for the details section under the university class tab of the new project form. It also permits the `course_title` and `course_number` in the `ProjectsController`.

---
+++ spec/views/projects/_university_class_form.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "app/views/projects/_university_class_form.html.erb", type: :view do
+  it "has the required fields in the 'Details' section" do
+    presenter = ProjectsNewPresenter.new(
+      user: :fake_user,
+      current_step: 1,
+    )
+
+    FakeForm.fields_for(ProjectForm.new) do |form|
+      render partial: "projects/university_class_form",
+        locals: { presenter: presenter, form: form }
+    end
+
+    doc = Capybara.string(rendered)
+    expect(doc).to have_field("Project or Event Title", type: "text")
+    expect(doc).to have_field("Course Title", type: "text")
+    expect(doc).to have_field("Course Number", type: "text")
+  end
+end
