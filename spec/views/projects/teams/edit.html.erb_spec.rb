fb7eae96b9a88975eacf03ea4d42a2121e96ac35
Ethan Fertsch <efertsch@gmail.com>
2021-12-09 10:57:01 -0500

    Add markup to teams page of project form
    This commit adds the markup for the second page (team) of the new project form. This is purely to structure the page, the necessary behaviors to make this page fully functional or styled have not yet been added.

--- spec/views/projects/teams/edit.html.erb_spec.rb
+++ spec/views/projects/teams/edit.html.erb_spec.rb
@@ -15,5 +15,20 @@ RSpec.describe "edit.html.erb" do
       expect(doc).to have_css("section.progress-steps")
       expect(doc).to have_css(".progress-steps li.active:nth-child(2)")
     end
+
+    it "displays a form to add a project team member" do
+      assign(:presenter, Projects::TeamsEditPresenter.new(
+        user: :dummy,
+        current_step: 2,
+        project: build(:project),
+      ))
+
+      render template: "projects/teams/edit"
+
+      doc = Capybara.string(rendered)
+      expect(doc).to have_css("form")
+      expect(doc).to have_field("Full name", type: "text")
+      expect(doc).to have_field("Project role", type: "select")
+    end
   end
- end
+end
