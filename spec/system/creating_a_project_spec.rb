b3ba79b82fd4050ea132e9df7220e09078a30eaa
Jon Yurek <jyurek@gmail.com>
2022-01-03 15:08:52 -0500

    Specify modal dismissal better in System Specs
    While `sleep`ing for an "inexpliable" amount of time seemed to work until now,
    finding the negative and letting Capybara do what it's supposed to works
    better. This removes the "inexplicble" wait and uses `has_no_css?` instead.
    
    This also changes the `showing_popup_*?` methods to include
    `not_showing_popup_*?` variants, because simply negating a capybara matcher
    doesn't work like you may expect.

--- spec/system/creating_a_project_spec.rb
+++ spec/system/creating_a_project_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Creating a project", type: :system, js: true do
 
       flow.dismiss_modal
 
-      expect(flow).not_to have_modal_displayed
+      expect(flow).to have_no_modal_displayed
       expect(page).to be_axe_clean
     end
   end
@@ -94,8 +94,7 @@ RSpec.describe "Creating a project", type: :system, js: true do
       institution: institution
     )
     flow.save_project_team_member
-    flow.inexplicably_sleep_for_a_tiny_amount_of_time_because_wait_is_insufficient
-    expect(flow).to_not be_showing_popup_creating_user
+    expect(flow).to be_not_showing_popup_creating_user
     expect(flow).to have_team_member("First Last")
 
     flow.enter_name_into_autocomplete("Anot")
@@ -111,14 +110,12 @@ RSpec.describe "Creating a project", type: :system, js: true do
 
     flow.change_users_role_to("Professional")
     flow.save_project_team_member
-    flow.inexplicably_sleep_for_a_tiny_amount_of_time_because_wait_is_insufficient
-    expect(flow).to_not be_showing_popup_editing_user("Another User")
+    expect(flow).to be_not_showing_popup_editing_user("Another User")
     expect(flow).to be_showing_user_role_as("Professional", for_user: "Another User")
 
     flow.edit_team_member("Another User")
     flow.remove_team_member
-    flow.inexplicably_sleep_for_a_tiny_amount_of_time_because_wait_is_insufficient
-    expect(flow).not_to have_team_member("Another User")
+    expect(flow).to have_no_team_member("Another User")
 
     flow.submit_step_two
     expect(flow).to be_on_project_permits_page
