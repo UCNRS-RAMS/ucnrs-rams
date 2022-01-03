6668301eaa069971a75c4e41c87da70b62a6a68c
Jon Yurek <jyurek@gmail.com>
2022-01-03 12:52:56 -0500

    Display new project progress bar on page 3 of form
    As per the other pages, this commit adds the start for the page view and
    presenter such that we can show the progress bar, which we can set to page 3
    now.

---
+++ spec/views/projects/permits/index.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "app/views/projects/permits/index.html.erb" do
+  describe "on any render" do
+    it "includes steps (on step 3)" do
+      assign(:presenter, Projects::PermitsIndexPresenter.new(
+        project: :dummy,
+        current_step: 3,
+      ))
+
+      render template: "projects/permits/index"
+
+      doc = Capybara.string(rendered)
+      expect(doc).to have_css("section.progress-steps")
+      expect(doc).to have_css(".progress-steps li.active:nth-child(3)")
+    end
+  end
+end
