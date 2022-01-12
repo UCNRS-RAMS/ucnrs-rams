35fb7856b4c23ccae0431a8766461c3af4b7e619
Ethan Fertsch <efertsch@gmail.com>
2022-01-12 18:52:24 -0500

    Display new project progress bar on step 4
    This commit adds the progress bar on the fourth (and last) page of the new project form. It also ensures that the correct active step is displayed based on the page number. This work required some basic presenter set up as @presenter is accessed in the view.

--- app/controllers/projects/fundings_controller.rb
+++ app/controllers/projects/fundings_controller.rb
@@ -1,7 +1,15 @@
 class Projects::FundingsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @presenter = Projects::FundingsIndexPresenter.new(
+      current_step: 4,
+      project: project,
+    )
+    respond_to do |format|
+      format.html
+    end
+  end
 
   private
 
---
+++ app/presenters/projects/fundings_index_presenter.rb
@@ -0,0 +1,14 @@
+class Projects::FundingsIndexPresenter
+  def initialize(current_step:, project:)
+    @current_step = current_step
+    @steps_presenter = StepsPresenter.new(@current_step)
+    @project = project
+  end
+
+  attr_reader :project
+  delegate :svg, :step_class, to: :steps_presenter
+
+  private
+
+  attr_reader :steps_presenter, :current_step
+end
