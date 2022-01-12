44b94ace7580327d7a522dbf2d22f041a4e5b7f8
Manuel M <manuelm@ucr.edu>
2022-01-11 19:00:44 -0800

    Add project summary page - left info bar
    This commit adds the left information bar for the project summary page.

---
+++ spec/views/projects/show.html.erb_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "show.html.erb" do
+  describe "on any render" do
+    it "includes sidebar" do
+      project = create(:project)
+      assign(:presenter, ProjectShowPresenter.new(project))
+
+      render template: "projects/show"
+
+      doc = Capybara.string(rendered)
+      expect(doc).to have_css("section.sidebar")
+      expect(doc).to have_css("ul.next-steps-list")
+      expect(doc).to have_css("a", text: "Schedule A Visit")
+    end
+  end
+end
