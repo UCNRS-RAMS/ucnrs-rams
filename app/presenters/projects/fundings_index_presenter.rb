200a1351dde3a5b0f2f8171d589def3280807a39
Jon Yurek <jyurek@gmail.com>
2022-01-14 09:57:05 -0500

    Markup and layout for "Add Funding" form
    Lay out and style the markup for the Add Funding form at the top of the
    Funding page of the Projects form.

--- app/presenters/projects/fundings_index_presenter.rb
+++ app/presenters/projects/fundings_index_presenter.rb
@@ -8,6 +8,18 @@ class Projects::FundingsIndexPresenter
   attr_reader :project
   delegate :svg, :step_class, to: :steps_presenter
 
+  def sponsor_options
+    [
+      "National Science Foundation (NSF)",
+      "National Institute of Health (NIH)",
+      "U.S. Geological Survey (USGS)",
+      "U.S. Forest Service (USFS)",
+      "U.S. Department of Agriculture (USDA)",
+      "California Department of Fish and Wildlife",
+      "Other"
+    ]
+  end
+
   private
 
   attr_reader :steps_presenter, :current_step
