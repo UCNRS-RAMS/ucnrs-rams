2a750b9deb65da7e6e891a1719d9493855106308
Manuel M <manuelm@ucr.edu>
2022-01-23 21:46:16 -0800

    Add project summary - visits
    This commit adds visit table to the project summary page.

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -52,6 +52,7 @@ RSpec.describe "show.html.erb" do
       expect(doc).to have_css("a", text: "Edit Funding")
       expect(doc).to have_css("table#funding-summary-table")
     end
+
     it "includes project permit section" do
       project = create(:project)
       assign(:presenter, ProjectShowPresenter.new(project))
@@ -63,6 +64,19 @@ RSpec.describe "show.html.erb" do
       expect(doc).to have_css("section.project-permit")
       expect(doc).to have_css("a", text: "Edit Permits")
       expect(doc).to have_css("div#permit-summary-list")
+    end
+    
+    it "includes project visits section" do
+      project = create(:project)
+      assign(:presenter, ProjectShowPresenter.new(project))
+
+      render template: "projects/show"
+
+      doc = Capybara.string(rendered)
+      
+      expect(doc).to have_css("section.project-visits")
+      expect(doc).to have_css(".content a", text: "Schedule A Visit")
+      expect(doc).to have_css("table#project-visit-requests")
     end
   end
 end
