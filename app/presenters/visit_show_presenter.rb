93e94edc31383a91d1706df5e8e3f2907a5dad2f
Manuel M <manuelm@ucr.edu>
2022-02-03 01:42:55 -0800

    Add Visit Summary - content top
    This commit adds the content top information to the visit summary page.

--- app/presenters/visit_show_presenter.rb
+++ app/presenters/visit_show_presenter.rb
@@ -26,10 +26,48 @@ class VisitShowPresenter
     "visits/sidebar_#{status}_show"
   end
 
+  def content_partial_name
+    if visit.approved?
+      "visits/content_approved_show"
+    else
+      "visits/content_show"
+    end
+  end
+
   def visit_reserve_personnel
     reserve.personnel.includes([:avatar_attachment]).map do |personnel|
       ReservePersonnelPresenter.new(personnel)
     end
+  end
+
+  delegate :id, to: :visit, prefix: true
+  delegate :name, to: :reserve, prefix: true
+  delegate :title, :project_type, to: :project, prefix: true
+
+  delegate_missing_to :visit
+
+  def submitted_at
+    visit.submitted_at ? I18n.l(visit.submitted_at, format: :visit_summary_time) : ""
+  end
+
+  def timeframe
+    if visit_timeframe_present?
+      "#{I18n.l(starts_at, format: :visit_summary_time)} - #{I18n.l(ends_at, format: :visit_summary_time)}"
+    else
+      not_applicable
+    end
+  end
+
+  def project_type
+    "#{project_project_type.capitalize} Project"
+  end
+
+  def visitor_count
+    user_visits.sum(&:count)
+  end
+
+  def amenity_count
+    amenity_visits.pluck(:amenity_id).uniq.length
   end
 
   private
@@ -38,5 +76,13 @@ class VisitShowPresenter
 
   def visit_reserve
     ReservePresenter.new(reserve)
+  end
+
+  def visit_timeframe_present?
+    starts_at.present? && starts_at.present?
+  end
+
+  def not_applicable
+    I18n.t(".projects.project.not_applicable")
   end
 end
