87c5cbde49025e55af5f18fa687dcdb16a64481d
Ethan Fertsch <efertsch@gmail.com>
2022-02-07 19:39:06 -0500

    Always display something if answer is affirmative
    This commit modifies the project summary page to ensure that there is always something displayed if a permit question was answered in the affirmative. Previously, if there was no statement, bullets for each answered question were being displayed with nothing beside them. Per the ticket:
    
    > In the real world, this should not be an issue because the permit questions will just be copied into the statement field in the event that a permit is missing a value for that column.
    
    This work basically ensures that the question will be shown as a fallback without requiring a data migration in the immediate.

--- app/presenters/project_permit_answer_presenter.rb
+++ app/presenters/project_permit_answer_presenter.rb
@@ -3,9 +3,14 @@ class ProjectPermitAnswerPresenter
     @project_permit_answer = project_permit_answer
   end
 
-  delegate :statement, to: :permit, prefix: true
+  delegate :permit, to: :project_permit_answer
+  delegate :statement, :question, to: :permit
 
   delegate_missing_to :project_permit_answer
+
+  def permit_statement
+    statement || question
+  end
 
   private
 
