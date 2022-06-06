92b8fbfdde9b96f9453ef2df563598a9c6b8aeb8
Manuel M <manuelm@ucr.edu>
2022-06-06 13:26:24 -0700

    Add confirm_manager.
    This commit adds the helper confirm_manager! to check if current_user is manager of the reserve.

--- app/controllers/manager/reserve_info/reserve_details_controller.rb
+++ app/controllers/manager/reserve_info/reserve_details_controller.rb
@@ -1,6 +1,7 @@
 class Manager::ReserveInfo::ReserveDetailsController < ApplicationController
   layout "manager"
   before_action :authenticate_user!
+  before_action :confirm_manager!
 
   def index
     @presenter = Manager::ReserveInfo::ReserveDetailsIndexPresenter.new(
--- spec/helpers/session_helper_spec.rb
+++ spec/helpers/session_helper_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe SessionHelper, type: :helper do
       expect(current_reserve).to eq(reserve)
     end
   end
+
+  describe "#confirm_manager!" do
+    it "returns true if current_user is a manager of the current_reserve" do
+      user = build(:user)
+      reserve = build(:reserve)
+      allow(helper).to receive(:current_user).and_return(user)
+      allow(helper).to receive(:current_reserve).and_return(reserve)
+      create(:reserve_personnel, reserve: reserve, user: user)
+
+      expect(helper.confirm_manager!).to eq true
+    end
+  end
 end
