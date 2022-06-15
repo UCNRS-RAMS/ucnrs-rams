d5e92f9e45de564c7fb38895d16c6166f51b3985
Hafiz Ahmad <ahmad.hafiz@gnar.dog>
2022-06-15 11:25:17 -0700

    Use predicate methods in system specs
    - Format menu bar links and indentation
    - Modify specs for manager project show page

--- spec/support/flows/manager/project_show_flow.rb
+++ spec/support/flows/manager/project_show_flow.rb
@@ -17,6 +17,58 @@ class ProjectShowFlow
     page.click_link("details")
   end
 
+  def showing_summary_box?
+    page.has_css?(".project-summary-box")
+  end
+
+  def showing_menu_bar?
+    page.has_css?(".project-menu-bar")
+  end
+
+  def has_section?(section)
+    page.has_css?("section.#{section}")
+  end
+
+  def has_navigation_link?(text)
+    page.has_css?("a", text: text)
+  end
+
+  def showing_summary_table?
+    page.has_css?("table#team-summary-table")
+  end
+
+  def has_no_inactive_user?
+    page.has_no_css?(".inactive-user")
+  end
+
+  def has_inactive_user?
+    page.has_css?(".inactive-user")
+  end
+
+  def showing_funding_summary_table?
+    page.has_css?("table#funding-summary-table")
+  end
+
+  def showing_permit_summary_list?
+    page.has_css?("div#permit-summary-list")
+  end
+
+  def showing_text?(text)
+    page.has_css?("p", text: text)
+  end
+
+  def showing_form?(form)
+    page.has_css?(form)
+  end
+
+  def not_showing_form?(form)
+    page.has_no_css?(form)
+  end
+
+  def has_heading?(heading)
+    page.has_css?("h3", text: heading)
+  end
+
   private
 
   attr_reader :page, :reserve_id, :project_id
