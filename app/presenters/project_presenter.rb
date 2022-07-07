7dd8643945243cc0e9cbfb4690667400c35b90cd
Manuel M <manuelm@ucr.edu>
2022-07-06 17:34:32 -0700

    Fix projects_index listing
    Added conditional checks for finding project.recent_visit_date. visit.start_date must be present.

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -29,7 +29,7 @@ class ProjectPresenter
   end
 
   def recent_visit_date
-    if visits.exists?
+    if visits.exists? && most_recent_visit.start_date.present?
       I18n.l(most_recent_visit.start_date, format: :project_visit_start_date)
     else
       not_applicable
