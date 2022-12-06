2aefe3b5e8ea2a18b5d7d2a965b7afa3825c2101
Hafiz Ahmad <ahmad.hafiz@gnar.dog>
2022-12-06 10:27:27 -0500

    Edit invoice payment records

--- app/forms/invoice_payment_form.rb
+++ app/forms/invoice_payment_form.rb
@@ -1,25 +1,34 @@
 # frozen_string_literal: true
 
 class InvoicePaymentForm
+  include Rails.application.routes.url_helpers
   include ActiveModel::Model
 
   def model_name
     ActiveModel::Name.new(InvoicePayment)
   end
 
-  def initialize(params: {})
-    @invoice_payment = InvoicePayment.find_by(id: params[:id]) || InvoicePayment.new(params)
+  def initialize(params: {}, editing: false)
+    @editing = editing
+    @invoice_payment = InvoicePayment.find_by(id: params[:id]) || InvoicePayment.new
+    assign(params)
   end
 
-  attr_reader :invoice_payment
+  attr_reader :invoice_payment, :editing
 
-  delegate :invoice_id, to: :invoice_payment
+  delegate_missing_to :invoice_payment
 
   def save
     invoice_payment.save if validate
   end
 
   private
+
+  def assign(params)
+    params.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
 
   def validate
     invoice_payment.valid?
