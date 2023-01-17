6e4eeaa45091de02205d251a098d5ff7b5b55aff
Hafiz Ahmad <ahmad.hafiz@gnar.dog>
2023-01-17 07:01:46 -0900

    Fix suggestions

--- app/models/reserve_tag.rb
+++ app/models/reserve_tag.rb
@@ -1,13 +1,13 @@
 class ReserveTag < ApplicationRecord
   belongs_to :reserve
 
-  enum tag_type: {
-    ecosystem: "Ecosystem",
-    geographic: "Geographic",
-    organization: "Organization",
-    amenities: "Amenities",
-    internet: "Internet",
-    other: "Other",
-    facility: "Facility",
+  enum category: {
+    ecosystem: "ecosystem",
+    geographic: "geographic",
+    organization: "organization",
+    amenities: "amenities",
+    internet: "internet",
+    other: "other",
+    facility: "facility",
   }
 end
