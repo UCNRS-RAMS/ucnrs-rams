057b413cdc91d88a90c3c445df517a1828a3321a
Hafiz Ahmad <ahmad.hafiz@gnar.dog>
2023-01-17 10:47:27 -0330

    Add spec to test the behavior of acts_as_paranoid.

--- spec/models/invoice_recipient_spec.rb
+++ spec/models/invoice_recipient_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe InvoiceRecipient do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:invoice) }
   end
+
+  describe "destroy" do
+    it "will do soft deletion for invoice_recipient" do
+      invoice_recipient = create(:invoice_recipient)
+
+      expect(invoice_recipient.deleted_at).to eq nil
+
+      invoice_recipient.destroy
+
+      expect(invoice_recipient.deleted_at).not_to eq nil
+    end
+  end
 end
