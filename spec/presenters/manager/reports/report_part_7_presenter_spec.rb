f2ad3f981e34e285806a638a8925b2eedbbf8108
Manuel M <manuelm@ucr.edu>
2023-01-31 00:16:46 -0800

    Add Report part 7
    This commit add Manager Report part 7 page.

---
+++ spec/presenters/manager/reports/report_part_7_presenter_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe Manager::Reports::ReportPart7Presenter do
+  describe "#fiscal_year_select_path" do
+    it "returns the path for the fiscal year dropdown" do
+      presenter = Manager::Reports::ReportPart7Presenter.new
+
+      fiscal_year_select_path = presenter.fiscal_year_select_path
+
+      expect(fiscal_year_select_path).to eq :report_part_7_manager_reserve_report_path
+    end
+  end
+
+  describe "#annual_report_column" do
+    it "returns the annual report column to update" do
+      presenter = Manager::Reports::ReportPart7Presenter.new
+
+      annual_report_column = presenter.annual_report_column
+
+      expect(annual_report_column).to eq :part_7_approved
+    end
+  end
+end
--- spec/presenters/manager/reports_report_part_7_presenter_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Manager::ReportsReportPart7Presenter do
-  describe "delegations" do
-    subject { Manager::ReportsReportPart7Presenter.new(report: build(:annual_report)) }
-    it { is_expected.to delegate_method(:fiscal_year_ending).to(:report) }
-  end
-
-end
