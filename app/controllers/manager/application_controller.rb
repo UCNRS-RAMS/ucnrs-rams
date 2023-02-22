69524df43e2e143ad52a4424f3885ae2f1ac97b7
Manuel M <manuelm@ucr.edu>
2023-02-22 01:46:52 -0800

    Add respond to turbo_frame modal request.
    This commit adds a way to display a flash alert when responding to both regular html request and html modal turbo_frame request.

--- app/controllers/manager/application_controller.rb
+++ app/controllers/manager/application_controller.rb
@@ -2,22 +2,35 @@ class Manager::ApplicationController < ApplicationController
   def confirm_current_reserve_manager!
     return true if current_user.manager_of_reserve?(current_reserve)
 
-    flash[:notice] = I18n.translate("manager.not_a_manager_of_reserve")
-    redirect_to root_url and return false
+    respond_to_modal_turbo_frame(flash_msg: I18n.translate("manager.not_a_manager_of_reserve"))
   end
 
   def is_administrator_or_accountant!
-    return true if current_reserve.personnel.role(current_user) == "Administrator" ||
-      current_reserve.personnel.role(current_user) == "Accountant"
+    return true if current_reserve.personnel.role(current_user).in? ["Administrator", "Accountant"]
 
-    flash[:alert] = I18n.translate("manager.not_authorize")
-    redirect_to root_url and return false
+    respond_to_modal_turbo_frame(flash_msg: I18n.translate("manager.not_authorize"))
   end
 
   def is_administrator!
-    return true if current_reserve.personnel.role(current_user) == "Administrator"
+    return true if current_reserve.personnel.role(current_user).in? ["Administrator"]
 
-    flash[:alert] = I18n.translate("manager.not_authorize")
-    redirect_to root_url and return false
+    respond_to_modal_turbo_frame(flash_msg: I18n.translate("manager.not_reserve_administrator"))
+  end
+
+  private
+
+  def respond_to_modal_turbo_frame(flash_msg: nil)
+    respond_to do |format|
+      format.html do |variant|
+        variant.turbo_frame {
+          flash.now[:alert] = flash_msg
+          render partial: "modals/flash", status: :unprocessable_entity and return
+        }
+        variant.none {
+          flash[:alert] = flash_msg
+          redirect_back fallback_location: root_url and return
+        }
+      end
+    end
   end
 end
