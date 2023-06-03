d64f5ab65177172b8b7b9fa7f08b0ab518af29f5
Manuel M <manuelm@ucr.edu>
2023-06-03 03:23:24 -0700

    Fix news articles
    Fix the news articles on the user home page.
    The wordpress api changed removing a field that was needed for finding the article picture and thus breaking the page. This commit added checks to see if the field exist before trying to display.

--- app/presenters/home/latest_news/index_presenter.rb
+++ app/presenters/home/latest_news/index_presenter.rb
@@ -1,17 +1,18 @@
 class Home::LatestNews::IndexPresenter
   LATEST_NEWS_URL = 'https://ucnrs.org/wp-json/wp/v2/posts'.freeze
   NUM_OF_NEWS = 3
+  DEFAULT_IMG = "fake-article-image-1.jpg"
 
   def initialize(getter)
     @getter = getter
   end
 
   def news_articles
-    NUM_OF_NEWS.times.map do |i|
+    body.each.map do |article|
       OpenStruct.new(
-        title: response.body[i]["title"]["rendered"],
-        link: response.body[i]["link"],
-        image: response.body[i]["uagb_featured_image_src"]["medium_large"][0]
+        title: article.title&.rendered,
+        link: article.link,
+        image: article.uagb_featured_image_src&.medium_large&.first || DEFAULT_IMG
       )
     end
   end
@@ -22,5 +23,9 @@ class Home::LatestNews::IndexPresenter
 
   def response
     @response ||= getter.get(url: LATEST_NEWS_URL, params: { per_page: NUM_OF_NEWS })
+  end
+
+  def body
+    @body ||= JSON.parse(response.body, object_class: OpenStruct)
   end
 end
--- spec/presenters/home/latest_news/index_presenter_spec.rb
+++ spec/presenters/home/latest_news/index_presenter_spec.rb
@@ -3,36 +3,37 @@ require "rails_helper"
 RSpec.describe Home::LatestNews::IndexPresenter do
   describe "#news_articles" do
     it "returns latest news" do
+      body = [
+        {
+          "title" => {
+            "rendered" => "title 1"
+          },
+          "link" => "link 1",
+          "uagb_featured_image_src"=> {
+            "medium_large" => ["image 1"],
+          }
+        },
+        {
+          "title" => {
+            "rendered" => "title 2"
+          },
+          "link" => "link 2",
+          "uagb_featured_image_src"=> {
+            "medium_large" => ["image 2"],
+          }
+        },
+        {
+          "title" => {
+            "rendered" => "title 3"
+          },
+          "link" => "link 3",
+          "uagb_featured_image_src"=> {
+            "medium_large" => ["image 3"],
+          }
+        },
+      ].to_json
       articles = OpenStruct.new(
-        body: [
-          {
-            "title" => {
-              "rendered" => "title 1"
-            },
-            "link" => "link 1",
-            "uagb_featured_image_src"=> {
-              "medium_large" => ["image 1"],
-            }
-          },
-          {
-            "title" => {
-              "rendered" => "title 2"
-            },
-            "link" => "link 2",
-            "uagb_featured_image_src"=> {
-              "medium_large" => ["image 2"],
-            }
-          },
-          {
-            "title" => {
-              "rendered" => "title 3"
-            },
-            "link" => "link 3",
-            "uagb_featured_image_src"=> {
-              "medium_large" => ["image 3"],
-            }
-          },
-        ]
+        body: body
       )
       getter = HttpGetter
       allow(getter).to receive(:get).and_return(articles)
