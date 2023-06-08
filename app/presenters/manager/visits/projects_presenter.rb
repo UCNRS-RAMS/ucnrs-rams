7e3629aeb7b1aa50b047d8b34a37071c0431bc6e
Manuel M <manuelm@ucr.edu>
2023-06-08 15:10:15 -0700

    Update projects_presenter.rb
    Remove user team_member permission check for when manager is creating visit for user.

--- app/presenters/manager/visits/projects_presenter.rb
+++ app/presenters/manager/visits/projects_presenter.rb
@@ -2,4 +2,14 @@ class Manager::Visits::ProjectsPresenter < Visits::ProjectsPresenter
   def initialize(project_type:, user:, project_id: nil)
     super(project_type: project_type, user: user, project_id: project_id)
   end
+
+  private
+
+  def projects_scope
+    Project
+      .of_type(project_type)
+      .alphabetized
+      .with_active_team_member(user: user, can_add_visit: false)
+      .for_status("Active Projects")
+  end
 end
