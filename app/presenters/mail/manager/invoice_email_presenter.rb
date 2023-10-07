1a9e19969755232fe1bf8d3ff6bfbac450defb57
ucnrs-manuel <manuelm@ucr.edu>
2023-10-06 23:48:56 -0700

    Add copy of invoice pdf to the invoice mail
    This commit adds pdf file to the invoice email. Additionally this commit also adds mail preview for invoice email

---
+++ app/presenters/mail/manager/invoice_email_presenter.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class Mail::Manager::InvoiceEmailPresenter
+  def initialize(invoice:, email_params:)
+    @invoice = invoice
+    @email_params = email_params
+  end
+
+  def email_to
+    email_params[:email_to_list]
+  end
+
+  def email_cc
+    if email_params[:cc_personnel]
+      personnel_receiving_invoice_email.map(&:email)
+
+    else
+      []
+
+    end
+  end
+
+  def email_subject
+    email_params[:subject] || email_params[:default_subject]
+  end
+
+  def email_body
+    email_params[:body]
+  end
+
+  def email_attachment_name
+    email_params[:attachment_name]
+  end
+
+  def email_attachment
+    email_params[:attachment]
+  end
+
+  private
+
+  attr_reader :email_params, :invoice
+
+  def personnel_receiving_invoice_email
+    ReservePersonnel
+      .where(
+        reserve: invoice.visit.reserve,
+        receive_invoice_email: true,
+      )
+      .map { |personnel| PersonnelPresenter.new(personnel) }
+  end
+end
