ecf923ddc9163d7ba4939deb2c978142d93124a5
ucnrs-manuel <manuelm@ucr.edu>
2023-10-11 19:15:15 -0700

    Fix news articles image
    Wordpress changed the way their API works regarding how to access the featured media for an article. This commit updates the api calls to access the images once again.

--- app/presenters/home/latest_news/index_presenter.rb
+++ app/presenters/home/latest_news/index_presenter.rb
@@ -1,18 +1,24 @@
 class Home::LatestNews::IndexPresenter
   LATEST_NEWS_URL = 'https://ucnrs.org/wp-json/wp/v2/posts'.freeze
   NUM_OF_NEWS = 3
-  DEFAULT_IMG = "fake-article-image-1.jpg"
+  DEFAULT_IMG = "fake-article-image-1.jpg".freeze
 
   def initialize(getter)
     @getter = getter
   end
 
   def news_articles
-    body.each.map do |article|
+    response = get_response url: LATEST_NEWS_URL, params: { per_page: NUM_OF_NEWS }
+    articles = json_to_object(response.body)
+
+    articles. each.map do |article|
+      response = get_response url: article._links["wp:featuredmedia"].first.href
+      featured_media = json_to_object(response.body)
+
       OpenStruct.new(
         title: article.title&.rendered,
         link: article.link,
-        image: article.uagb_featured_image_src&.medium_large&.first || DEFAULT_IMG
+        image: featured_media&.media_details&.sizes&.medium_large&.source_url || DEFAULT_IMG
       )
     end
   end
@@ -21,11 +27,11 @@ class Home::LatestNews::IndexPresenter
 
   attr_reader :getter
 
-  def response
-    @response ||= getter.get(url: LATEST_NEWS_URL, params: { per_page: NUM_OF_NEWS })
+  def get_response(url: nil, params: nil)
+    getter.get(url: url, params: params)
   end
 
-  def body
-    @body ||= JSON.parse(response.body, object_class: OpenStruct)
+  def json_to_object(json)
+    JSON.parse(json, object_class: OpenStruct)
   end
 end
