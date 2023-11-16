b6b8f2875ce41548878c81b8a320e7b501785a3e
ucnrs-manuel <manuelm@ucr.edu>
2023-11-15 23:26:34 -0800

    Update annual report part 1 presenter
    This commit updates the annual report part 1 presenter.

--- spec/presenters/manager/reports/report_part_1_presenter_spec.rb
+++ spec/presenters/manager/reports/report_part_1_presenter_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Manager::Reports::ReportPart1Presenter do
     subject { Manager::Reports::ReportPart1Presenter.new(form: :form) }
     it { is_expected.to delegate_method(:annual_report).to(:form).with_prefix(true) }
     it { is_expected.to delegate_method(:fiscal_year_ending).to(:form_annual_report) }
+    it { is_expected.to delegate_method(:reserve).to(:form_annual_report).with_prefix(true) }
+    it { is_expected.to delegate_method(:managing_campus).to(:form_annual_report_reserve).with_prefix(true) }
   end
 
   describe "#report_part1" do
@@ -74,6 +76,33 @@ RSpec.describe Manager::Reports::ReportPart1Presenter do
       fiscal_year_ending_options = presenter.fiscal_year_ending_options
 
       expect(fiscal_year_ending_options).to match_array fiscal_year_options
+    end
+  end
+
+  describe "#reserve_name" do
+    it "is the form annual_report associated reserve name" do
+      reserve = create(:reserve, name: "Reserve1")
+      annual_report = create(:annual_report, reserve: reserve)
+      form = AnnualReportForm.new(annual_report: annual_report)
+      presenter = Manager::Reports::ReportPart1Presenter.new(form: form)
+
+      reserve_name = presenter.reserve_name
+
+      expect(reserve_name).to eq "Reserve1"
+    end
+  end
+
+  describe "#reserve_managing_campus_name" do
+    it "is the form annual_report associated reserve managing campus name" do
+      institution = create(:institution, name: "Institution2")
+      reserve = create(:reserve, managing_campus: institution)
+      annual_report = create(:annual_report, reserve: reserve)
+      form = AnnualReportForm.new(annual_report: annual_report)
+      presenter = Manager::Reports::ReportPart1Presenter.new(form: form)
+
+      reserve_managing_campus_name = presenter.reserve_managing_campus_name
+
+      expect(reserve_managing_campus_name).to eq "Institution2"
     end
   end
 end
