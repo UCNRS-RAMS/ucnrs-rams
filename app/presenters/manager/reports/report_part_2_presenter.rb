2537147a115530216b379cc2cec69ac9d44fbe50
ucnrs-manuel <manuelm@ucr.edu>
2023-12-04 14:52:46 -0800

    Add pdf to annual report part 2
    This commit adds pdf version of annual report part 2

--- app/presenters/manager/reports/report_part_2_presenter.rb
+++ app/presenters/manager/reports/report_part_2_presenter.rb
@@ -1,29 +1,8 @@
-class Manager::Reports::ReportPart2Presenter
-  def initialize(form: nil, reserve: nil)
-    @form = form
-    @reserve = reserve
-  end
-
-  delegate :annual_report, to: :form, prefix: true
-
-  delegate :fiscal_year_ending, to: :form_annual_report
-
-  attr_reader :form
-
+class Manager::Reports::ReportPart2Presenter < Manager::Reports::ReportBasePresenter
   def report_part2_data
     report_part2_data_scope
       .map{ |institution| InstitutionPresenter.new(institution) }
       .group_by(&:institution_type)
-  end
-
-  def fiscal_year
-    "#{fiscal_year_ending - 1}-#{fiscal_year_ending}"
-  end
-
-  def fiscal_year_ending_options
-    [].tap do |arr|
-      (2000..(Date.current.year + 1)).each { |year_end| arr << ["#{year_end - 1}-#{year_end}", year_end] }
-    end
   end
 
   def fiscal_year_select_path
@@ -39,7 +18,7 @@ class Manager::Reports::ReportPart2Presenter
       .joins(:user_visits)
       .merge(
         UserVisit
-          .at_reserve(@reserve)
+          .at_reserve(form_annual_report_reserve)
           .approved_status
           .having_between_time(date_start: start_date, date_end: stop_date)
           .joins(:visit)
@@ -50,15 +29,5 @@ class Manager::Reports::ReportPart2Presenter
       .group(:id)
       .order(:institution_type, :name)
       .includes([:state, :country])
-  end
-
-  private
-
-  def start_date
-    Date.new(fiscal_year_ending - 1, 7, 1)
-  end
-
-  def stop_date
-    Date.new(fiscal_year_ending, 6, 30)
   end
 end
--- spec/presenters/manager/reports/report_part_2_presenter_spec.rb
+++ spec/presenters/manager/reports/report_part_2_presenter_spec.rb
@@ -1,12 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Manager::Reports::ReportPart2Presenter do
-  describe "delegations" do
-    subject { Manager::Reports::ReportPart2Presenter.new(form: :form) }
-    it { is_expected.to delegate_method(:annual_report).to(:form).with_prefix(true) }
-    it { is_expected.to delegate_method(:fiscal_year_ending).to(:form_annual_report) }
-  end
-
   describe "#report_part2_data" do
     it "group report part 2 data by 'project_type'" do
       travel_to Time.zone.local(2022, 2, 22)
@@ -71,8 +65,9 @@ RSpec.describe Manager::Reports::ReportPart2Presenter do
       )
       annual_report = double AnnualReport
       allow(annual_report).to receive(:fiscal_year_ending).and_return(Date.current.year)
+      allow(annual_report).to receive(:reserve).and_return(reserve1)
       form = AnnualReportForm.new(annual_report: annual_report)
-      presenter = Manager::Reports::ReportPart2Presenter.new(form: form, reserve: reserve1)
+      presenter = Manager::Reports::ReportPart2Presenter.new(form: form)
 
       report_part2_data = presenter.report_part2_data
 
@@ -87,31 +82,6 @@ RSpec.describe Manager::Reports::ReportPart2Presenter do
       expect(report_part2_data[:non_governmental_organization_or_entity.to_s].map(&:id)).to match_array [
         ngo_type_institution.id,
       ]
-    end
-  end
-
-  describe "#fiscal_year" do
-    it "is the form fiscal year" do
-      annual_report = create(:annual_report, fiscal_year_ending: 2049)
-      form = AnnualReportForm.new(annual_report: annual_report)
-      presenter = Manager::Reports::ReportPart2Presenter.new(form: form)
-
-      fiscal_year = presenter.fiscal_year
-
-      expect(fiscal_year).to eq "2048-2049"
-    end
-  end
-
-  describe "#fiscal_year_ending_options" do
-    it "is an array of fiscal year options" do
-      fiscal_year_options = [].tap do |arr|
-        (2000..(Date.current.year + 1)).each { |year_end| arr << ["#{year_end - 1}-#{year_end}", year_end] }
-      end
-      presenter = Manager::Reports::ReportPart2Presenter.new
-
-      fiscal_year_ending_options = presenter.fiscal_year_ending_options
-
-      expect(fiscal_year_ending_options).to match_array fiscal_year_options
     end
   end
 
@@ -168,15 +138,13 @@ RSpec.describe Manager::Reports::ReportPart2Presenter do
       )
       annual_report = double AnnualReport
       allow(annual_report).to receive(:fiscal_year_ending).and_return(Date.current.year)
+      allow(annual_report).to receive(:reserve).and_return(reserve1)
       form = AnnualReportForm.new(annual_report: annual_report)
-      presenter1 = Manager::Reports::ReportPart2Presenter.new(form: form, reserve: reserve1)
-      presenter2 = Manager::Reports::ReportPart2Presenter.new(form: form, reserve: reserve3)
+      presenter1 = Manager::Reports::ReportPart2Presenter.new(form: form)
 
       report_part2_data_scope1 = presenter1.report_part2_data_scope
-      report_part2_data_scope2 = presenter2.report_part2_data_scope
 
       expect(report_part2_data_scope1).to match_array [institution1, institution3]
-      expect(report_part2_data_scope2).to match_array []
     end
 
     it "returns only institutions having associated with approved status user_visits" do
@@ -206,8 +174,9 @@ RSpec.describe Manager::Reports::ReportPart2Presenter do
       )
       annual_report = double AnnualReport
       allow(annual_report).to receive(:fiscal_year_ending).and_return(Date.current.year)
+      allow(annual_report).to receive(:reserve).and_return(reserve1)
       form = AnnualReportForm.new(annual_report: annual_report)
-      presenter = Manager::Reports::ReportPart2Presenter.new(form: form, reserve: reserve1)
+      presenter = Manager::Reports::ReportPart2Presenter.new(form: form)
 
       report_part2_data_scope = presenter.report_part2_data_scope
 
@@ -249,8 +218,9 @@ RSpec.describe Manager::Reports::ReportPart2Presenter do
       )
       annual_report = double AnnualReport
       allow(annual_report).to receive(:fiscal_year_ending).and_return(Date.current.year)
+      allow(annual_report).to receive(:reserve).and_return(reserve1)
       form = AnnualReportForm.new(annual_report: annual_report)
-      presenter = Manager::Reports::ReportPart2Presenter.new(form: form, reserve: reserve1)
+      presenter = Manager::Reports::ReportPart2Presenter.new(form: form)
 
       report_part2_data_scope = presenter.report_part2_data_scope
 
@@ -292,8 +262,9 @@ RSpec.describe Manager::Reports::ReportPart2Presenter do
       )
       annual_report = double AnnualReport
       allow(annual_report).to receive(:fiscal_year_ending).and_return(Date.current.year)
+      allow(annual_report).to receive(:reserve).and_return(reserve1)
       form = AnnualReportForm.new(annual_report: annual_report)
-      presenter = Manager::Reports::ReportPart2Presenter.new(form: form, reserve: reserve1)
+      presenter = Manager::Reports::ReportPart2Presenter.new(form: form)
 
       report_part2_data_scope = presenter.report_part2_data_scope
 
@@ -338,8 +309,9 @@ RSpec.describe Manager::Reports::ReportPart2Presenter do
       )
       annual_report = double AnnualReport
       allow(annual_report).to receive(:fiscal_year_ending).and_return(Date.current.year)
+      allow(annual_report).to receive(:reserve).and_return(reserve1)
       form = AnnualReportForm.new(annual_report: annual_report)
-      presenter = Manager::Reports::ReportPart2Presenter.new(form: form, reserve: reserve1)
+      presenter = Manager::Reports::ReportPart2Presenter.new(form: form)
 
       report_part2_data_scope = presenter.report_part2_data_scope
 
@@ -377,8 +349,9 @@ RSpec.describe Manager::Reports::ReportPart2Presenter do
       )
       annual_report = double AnnualReport
       allow(annual_report).to receive(:fiscal_year_ending).and_return(Date.current.year)
+      allow(annual_report).to receive(:reserve).and_return(reserve1)
       form = AnnualReportForm.new(annual_report: annual_report)
-      presenter = Manager::Reports::ReportPart2Presenter.new(form: form, reserve: reserve1)
+      presenter = Manager::Reports::ReportPart2Presenter.new(form: form)
 
       report_part2_data_scope = presenter.report_part2_data_scope
 
