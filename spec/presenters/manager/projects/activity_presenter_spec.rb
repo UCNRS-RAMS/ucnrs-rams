b46d08d28199f29569ccffa87496dfa17d02b026
Manuel M <manuelm@ucr.edu>
2024-05-05 02:05:58 -0700

    update spec
    This commit updates a couple of spec to be inline with the rest of the coding convention used.

--- spec/presenters/manager/projects/activity_presenter_spec.rb
+++ spec/presenters/manager/projects/activity_presenter_spec.rb
@@ -1,10 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Manager::Projects::ActivityPresenter do
-  let(:user) { create(:user, :confirmed) }
-  let(:reserve) { create(:reserve) }
-  let(:project) { create(:project, reserve: reserve) }
-
   describe "#action_name" do
     it "should return the action name for log record based on about" do
       metadata = {
@@ -17,9 +13,9 @@ RSpec.describe Manager::Projects::ActivityPresenter do
           "submitted_at" => [nil, "2020-05-23T16:03:56.000-07:00"]
         }
       }
-      log1 = project.logs.create(user: user, reserve_id: reserve.id, metadata: metadata.to_json)
+      log1 = create(:log, metadata: metadata.to_json)
       metadata["about"] = "test"
-      log2 = project.logs.create(user: user, reserve_id: reserve.id, metadata: metadata.to_json)
+      log2 = create(:log, metadata: metadata.to_json)
       presenter1 = Manager::Projects::ActivityPresenter.new(record: log1)
       presenter2 = Manager::Projects::ActivityPresenter.new(record: log2)
 
@@ -31,7 +27,7 @@ RSpec.describe Manager::Projects::ActivityPresenter do
   describe "#date" do
     it "should return the formatted date with time" do
       travel_to Time.zone.local(2004, 11, 24, 1, 4, 44)
-      log = project.logs.create(user: user, reserve_id: reserve.id, created_at: Time.current)
+      log = create(:log, created_at: Time.current)
       presenter = Manager::Projects::ActivityPresenter.new(record: log)
 
       expect(presenter.date).to eq("Nov. 24, 2004 at  1:04 AM")
@@ -52,10 +48,11 @@ RSpec.describe Manager::Projects::ActivityPresenter do
 
   describe "#user_name" do
     it "should return the full name of the user" do
-      log = project.logs.create(user: user, reserve_id: reserve.id)
+      user = create(:user, first_name: "mr", last_name: "smith")
+      log = create(:log, user: user)
       presenter = Manager::Projects::ActivityPresenter.new(record: log)
 
-      expect(presenter.user_name).to eq(user.full_name)
+      expect(presenter.user_name).to eq("mr smith")
     end
   end
 end
