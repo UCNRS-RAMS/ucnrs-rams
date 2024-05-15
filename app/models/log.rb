d62ee90c31980485b87b40417d4826563149bf3a
Manuel M <manuelm@ucr.edu>
2024-05-15 02:21:47 -0700

    Add reserve to visit log
    This commit fills out the log reserve field for visit submission

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,6 +2,7 @@ class Log < ApplicationRecord
   belongs_to :record, polymorphic: true
   belongs_to :record_about, polymorphic: true, optional: true
   belongs_to :user
+  belongs_to :reserve, optional: true
 
   ACTIONS = {
     submitted: "submitted",
