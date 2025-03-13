142d29d30f1cf9917bedbb4e3fbdfd1758c62db1
Manuel M <manuelm@ucr.edu>
2025-03-12 18:20:18 -0700

    Fix project show link
    Fix link for public and manager side of project show page

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -60,12 +60,12 @@ RSpec.describe "show.html.erb" do
       render template: "projects/show"
 
       doc = Capybara.string(rendered)
-      
+
       expect(doc).to have_css("section.project-permit")
       expect(doc).to have_css("a", text: "Edit Permits")
       expect(doc).to have_css("div#permit-summary-list")
     end
-    
+
     it "includes project visits section" do
       project = create(:project)
       assign(:presenter, ProjectShowPresenter.new(project))
@@ -73,9 +73,9 @@ RSpec.describe "show.html.erb" do
       render template: "projects/show"
 
       doc = Capybara.string(rendered)
-      
+
       expect(doc).to have_css("section.project-visits")
-      expect(doc).to have_css(".content a", text: "Schedule a Visit")
+      expect(doc).to have_css(".content a", text: "Schedule a new Visit")
       expect(doc).to have_css("table#project-visit-requests")
     end
   end
