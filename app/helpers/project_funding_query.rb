2c78084d8b4fb14f63db1a630dda41e57082d810
Manuel M <manuelm@ucr.edu>
2025-09-08 17:54:55 -0700

    Fix flex project funding
    Fix query in flex project funding page.

--- app/helpers/project_funding_query.rb
+++ app/helpers/project_funding_query.rb
@@ -1,5 +1,5 @@
 module ProjectFundingQuery
-  def project_funding(reserve: nil, begin_date: nil, end_date: nil)
+  def project_funding(reserve: nil, date_begin: nil, date_end: nil)
 
     if reserve.present?
       reserve_query = "visits.reserve_id = #{reserve}"
@@ -7,18 +7,29 @@ module ProjectFundingQuery
       reserve_query = "visits.reserve_id = NULL"
     end
 
+    if date_begin.present?
+      date_begin = ActiveRecord::Base.sanitize_sql("'#{date_begin} 00:00:00'")
+    else
+      date_begin = "NULL"
+    end
+
+    if date_end.present?
+      date_end = ActiveRecord::Base.sanitize_sql("'#{date_end} 23:59:59'")
+    else
+      date_end = "NULL"
+    end
+
     Funding
       .select("fundings.*, visits.reserve_id")
       .left_outer_joins(project: [visits: :user_visits])
       .where(
         "#{reserve_query}
-        AND user_visits.arrives_at >= :start_date
-        AND user_visits.departs_at <= :stop_date
+        AND user_visits.departs_at >= #{date_begin}
+        AND user_visits.arrives_at <= #{date_end}
         AND user_visits.status = 'Approved'
         AND projects.project_type = 'Research'
         AND projects.AnnualReportAccess = 1
         AND projects.status IN ('Open', 'Closed')",
-        { start_date: begin_date, stop_date: end_date },
       )
       .includes(:project)
       .group(:id)
