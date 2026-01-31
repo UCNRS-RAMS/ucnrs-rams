a78a2cbd162bbc9c8fbfbaf035afdf1e8948027d
Manuel M <manuelm@ucr.edu>
2026-01-31 15:26:59 -0800

    Change ARpart8
    Change ARpart8 to display data more similar to ARpart3

--- spec/presenters/manager/reports/report_part_8/project_presenter_spec.rb
+++ spec/presenters/manager/reports/report_part_8/project_presenter_spec.rb
@@ -1,23 +1,102 @@
 require "rails_helper"
 
 RSpec.describe Manager::Reports::ReportPart8::ProjectPresenter do
-  describe "delegations" do
-    subject { Manager::Reports::ReportPart8::ProjectPresenter.new(:project) }
-    it { is_expected.to delegate_method(:role).to(:owner).with_prefix(true) }
-    it { is_expected.to delegate_method(:institution).to(:owner).with_prefix(true) }
-    it { is_expected.to delegate_missing_methods_to(:project) }
-  end
-
-  describe "#owner" do
-    it "presents the project user record wrapped in UserPresenter" do
+  describe "#timeframe" do
+    it "returns not applicable when there is no user visits under the project" do
       user = create(:user)
       project = create(:project, owner: user)
-      presenter = Manager::Reports::ReportPart8::ProjectPresenter.new(project)
+      create(:visit, project: project)
+      allow(I18n).to receive(:t)
+        .with(".projects.project.not_applicable")
+        .and_return("not_applicable_translate")
+      presenter = Manager::Reports::ReportPart8::ProjectPresenter.new(
+        project:, start_date: nil, stop_date: nil)
 
-      owner = presenter.owner
+      timeframe = presenter.timeframe
 
-      expect(owner.id).to eq user.id
-      expect(owner).to be_a(UserPresenter)
+      expect(timeframe).to eq "not_applicable_translate"
+    end
+
+    it "returns a timeframe when there is a user visit under the project" do
+      user = create(:user)
+      project = create(:project, owner: user)
+      visit = create(:visit, project: project)
+      user_visit = create(:user_visit,
+        visit: visit, arrives_at: 2.days.ago, departs_at: 2.days.from_now)
+      allow(DateRangePresenter).to receive(:value)
+      presenter = Manager::Reports::ReportPart8::ProjectPresenter.new(
+        project: project, start_date: 6.months.ago.to_date, stop_date: 6.months.from_now.to_date)
+
+      presenter.timeframe
+
+      expect(DateRangePresenter).to have_received(:value)
+        .with(start_date: user_visit.arrives_at.to_date, end_date: user_visit.departs_at.to_date)
+    end
+
+    context "when there are more than one user visits" do
+      it "returns a timeframe of the earliest arrival and latest departure" do
+        user = create(:user)
+        project = create(:project, owner: user)
+        visit = create(:visit, project: project)
+        user_visit1 = create(:user_visit,
+          visit: visit, arrives_at: 5.days.ago, departs_at: 3.days.ago)
+        user_visit2 = create(:user_visit,
+          visit: visit, arrives_at: 2.days.ago, departs_at: 2.days.from_now)
+        user_visit3 = create(:user_visit,
+          visit: visit, arrives_at: 5.days.from_now, departs_at: 7.days.from_now)
+        allow(DateRangePresenter).to receive(:value)
+        presenter = Manager::Reports::ReportPart8::ProjectPresenter.new(
+          project: project, start_date: 6.months.ago.to_date, stop_date: 6.months.from_now.to_date)
+
+        presenter.timeframe
+
+        expect(DateRangePresenter).to have_received(:value)
+          .with(start_date: user_visit1.arrives_at.to_date, end_date: user_visit3.departs_at.to_date)
+      end
+    end
+
+    context "when there the user visit arrival is earlier than report start date" do
+      it "returns a the report start date" do
+        user = create(:user)
+        project = create(:project, owner: user)
+        visit = create(:visit, project: project)
+        user_visit1 = create(:user_visit,
+          visit: visit, arrives_at: 5.days.ago, departs_at: 3.days.ago)
+        user_visit2 = create(:user_visit,
+          visit: visit, arrives_at: 7.months.ago, departs_at: 2.days.from_now)
+        user_visit3 = create(:user_visit,
+          visit: visit, arrives_at: 5.days.from_now, departs_at: 7.days.from_now)
+        allow(DateRangePresenter).to receive(:value)
+        presenter = Manager::Reports::ReportPart8::ProjectPresenter.new(
+          project: project, start_date: 6.months.ago.to_date, stop_date: 6.months.from_now.to_date)
+
+        presenter.timeframe
+
+        expect(DateRangePresenter).to have_received(:value)
+          .with(start_date: 6.months.ago.to_date, end_date: user_visit3.departs_at.to_date)
+      end
+    end
+
+    context "when there the user visit departure is later than report stop date" do
+      it "returns a the report stop date" do
+        user = create(:user)
+        project = create(:project, owner: user)
+        visit = create(:visit, project: project)
+        user_visit1 = create(:user_visit,
+          visit: visit, arrives_at: 5.days.ago, departs_at: 3.days.ago)
+        user_visit2 = create(:user_visit,
+          visit: visit, arrives_at: 2.days.ago, departs_at: 7.months.from_now)
+        user_visit3 = create(:user_visit,
+          visit: visit, arrives_at: 5.days.from_now, departs_at: 7.days.from_now)
+        allow(DateRangePresenter).to receive(:value)
+        presenter = Manager::Reports::ReportPart8::ProjectPresenter.new(
+          project: project, start_date: 6.months.ago.to_date, stop_date: 6.months.from_now.to_date)
+
+        presenter.timeframe
+
+        expect(DateRangePresenter).to have_received(:value)
+          .with(start_date: user_visit1.arrives_at.to_date, end_date: 6.months.from_now.to_date)
+      end
     end
   end
 end
