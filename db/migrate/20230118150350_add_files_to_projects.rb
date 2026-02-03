1877ab004a98f418c669da9f6f11534fd106ad61
Scott F <Scott.Fisher@ucop.edu>
2026-02-03 15:53:26 -0800

    Change and add migration to fix schema
    Restore missing migration file that makes dm:migrate fail if doing from the start!
    
    Add/change migrations in line with current in-use schema

--- db/migrate/20230118150350_add_files_to_projects.rb
+++ db/migrate/20230118150350_add_files_to_projects.rb
@@ -1,5 +1,11 @@
 class AddFilesToProjects < ActiveRecord::Migration[6.1]
   def change
     add_column :projects, :files, :json
+
+    # These indexes are to match the current schema.rb which didn't contain the migration for this
+    add_index :projects, ["reserve_id", "status", "id"], name: "Reserve"
+    add_index :projects, ["reserve_id"], name: "reserve_id"
+    remove_index :projects, name: "project_status"
+    add_index :projects, ["status", "reserve_id", "id"], name: "project_status"
   end
 end
