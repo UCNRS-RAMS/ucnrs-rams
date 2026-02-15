1ea08173369238ab8bc003aebcfdf89fc05ebf4a
Manuel M <manuelm@ucr.edu>
2026-02-15 02:03:35 -0800

    Update load_defaults
    Update load_defaults to 7.0 in preparation for upgrade to 7.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module UcnrsRams
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,10 +18,5 @@ module UcnrsRams
     #
     config.time_zone = "Pacific Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.action_view.automatically_disable_submit_tag = false
-
-    config.active_support.disable_to_s_conversion = true
-    config.active_support.cache_format_version = 7.0
   end
 end
