e5737357df40e85405d5bb3ca55584a4c5d0fb89
Manuel M <manuelm@ucr.edu>
2026-02-15 01:40:33 -0800

    Update _group.html.erb_spec.rb
    Fix broken spec because of rails upgrade to 7.1

--- spec/views/projects/questions/_group.html.erb_spec.rb
+++ spec/views/projects/questions/_group.html.erb_spec.rb
@@ -10,16 +10,10 @@ RSpec.describe "projects/questions/_group.html.erb", type: :view do
   end
 
   it "renders a partial for each question passed in" do
-    stub_template(
-      "projects/questions/_one.html.erb" => "<li>One!</li>",
-      "projects/questions/_two.html.erb" => "<li>Two!</li>",
-      "projects/questions/_three.html.erb" => "<li>Three!</li>",
-    )
-
     render partial: "projects/questions/group", locals: { title: "", questions: [
-      OpenStruct.new(render_values: { partial: "one" }),
-      OpenStruct.new(render_values: { partial:  "two" }),
-      OpenStruct.new(render_values: { partial:  "three" }),
+      double(render_values: { inline: "<li>One!</li>" }),
+      double(render_values: { inline: "<li>Two!</li>" }),
+      double(render_values: { inline: "<li>Three!</li>" }),
     ]}
 
     doc = Capybara.string(rendered)
