14f94d5be875c26e343c023e3ab5f481087e5ea0
Scott F <Scott.Fisher@ucop.edu>
2026-02-24 15:42:37 -0800

    Fix flapping tests that are intermittently failing because of race condition with mailer depending what finishes first.

--- spec/support/flows/authentication_flow.rb
+++ spec/support/flows/authentication_flow.rb
@@ -17,22 +17,17 @@ class AuthenticationFlow
   end
 
   def reset_password_for(email)
+    ActionMailer::Base.deliveries.clear
     page.fill_in("Email", with: email)
     page.find("input[type='submit']").click
   end
 
   def follow_reset_password_email_link
-    page.has_content?("You will receive an email")
-
-    email_delivery = ActionMailer::Base.deliveries.last
-    if email_delivery.nil?
-      raise "Password reset email was not delivered. Deliveries: #{ActionMailer::Base.deliveries.size}"
-    end
-
+    email_delivery = wait_for_last_email
     match = email_delivery.body.match(%r{/users/password/edit\?reset_password_token=[\w-]+})
-    if match.present?
-      page.visit(match[0])
-    end
+    raise "Reset password email missing token link" unless match.present?
+
+    page.visit(match[0])
   end
 
   def reset_password_to(new_password)
@@ -50,6 +45,7 @@ class AuthenticationFlow
   end
 
   def fill_out_account_creation_form(email:, password:)
+    ActionMailer::Base.deliveries.clear
     page.fill_in("Email", with: email)
     page.fill_in("Password", with: password)
     page.fill_in("Password confirmation", with: password)
@@ -59,15 +55,11 @@ class AuthenticationFlow
   def confirm_email
     page.has_content?("A message with a confirmation link")
 
-    email_delivery = ActionMailer::Base.deliveries.last
     if email_delivery.nil?
       raise "Confirmation email was not delivered. Deliveries: #{ActionMailer::Base.deliveries.size}"
     end
 
     match = email_delivery.body.match(%r{/users/confirmation\?confirmation_token=[\w-]+})
-    if match.present?
-      page.visit(match[0])
-    end
   end
 
   def sign_in_as(email:, password:)
@@ -112,4 +104,16 @@ class AuthenticationFlow
   private
 
   attr_reader :page
+
+  def wait_for_last_email(context_label = "reset password")
+    deadline = Time.now + Capybara.default_max_wait_time
+
+    loop do
+      email_delivery = ActionMailer::Base.deliveries.last
+      return email_delivery if email_delivery
+      raise "Timed out waiting for #{context_label} email" if Time.now >= deadline
+
+      sleep 0.1
+    end
+  end
 end
