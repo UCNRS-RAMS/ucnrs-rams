1abd9a3d1410dc14aa99011e572df44a20d753db
Manuel M <manuelm@ucr.edu>
2026-03-31 20:16:14 -0700

    Fix text beside logo
    This commit fix reserve institution and reserve name occupying same line instead of 2. Also tweak logo margins.

--- spec/presenters/manager/reserve_info/reserve_form_presenter_spec.rb
+++ spec/presenters/manager/reserve_info/reserve_form_presenter_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe Manager::ReserveInfo::ReserveFormPresenter do
       form = ReserveForm.new(reserve: reserve)
       presenter = Manager::ReserveInfo::ReserveFormPresenter.new(form)
 
-      expect(presenter.logo).to eq("ucnrs-logo.png")
+      expect(presenter.logo).to eq("ucnature-logo.png")
     end
 
     it "presents the correct avatar path if logo is attached" do
