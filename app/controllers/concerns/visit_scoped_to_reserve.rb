e7117d9a834cca8938e597847b9eb50387b7ee57
Manuel M <manuelm@ucr.edu>
2026-05-07 23:04:12 -0700

    Update flash message

--- app/controllers/concerns/visit_scoped_to_reserve.rb
+++ app/controllers/concerns/visit_scoped_to_reserve.rb
@@ -6,6 +6,12 @@ module VisitScopedToReserve
   def confirm_visit_in_reserve!
     return true if visit.reserve_id == current_reserve.id
 
-    respond_to_modal_turbo_frame(flash_msg: I18n.translate("manager.not_authorize"))
+    respond_to_modal_turbo_frame(
+      flash_msg: I18n.translate(
+        "manager.permission.visit_not_in_reserve",
+        id: visit.id,
+        reserve: current_reserve.name,
+      ),
+    )
   end
 end
