bf5390a602dfe987e204a6ec022e2c9abbf13b50
Scott F <Scott.Fisher@ucop.edu>
2026-06-05 09:19:50 -0700

    Fixes the unique key requirement for indexing and just adds as index to speed lookups without requiring uniqueness.

--- db/migrate/20260513121000_add_unique_index_to_institutions_name_city.rb
+++ db/migrate/20260513121000_add_unique_index_to_institutions_name_city.rb
@@ -2,7 +2,6 @@ class AddUniqueIndexToInstitutionsNameCity < ActiveRecord::Migration[7.2]
   def change
     add_index :institutions,
               [:name, :city],
-              unique: true,
               name: "index_institutions_on_name_and_city"
   end
 end
