0a6f545f1dc57ed1dcfb3cc9727d2f6e8709605a
Scott F <Scott.Fisher@ucop.edu>
2026-06-12 16:28:54 -0700

    Gets a working ORCID callback cycle at 127.0.0.1

--- app/javascript/controllers/orcid_controller.ts
+++ app/javascript/controllers/orcid_controller.ts
@@ -2,8 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["input"]
+  static values = { authPath: String, origin: String }
 
   declare inputTarget: HTMLInputElement
+  declare authPathValue: string
+  declare originValue: string
 
   connect() {
     const params = new URLSearchParams(window.location.search)
@@ -20,6 +23,32 @@ export default class extends Controller {
     params.delete("orcid_callback")
     params.delete("orcid_auth_error")
     this.replaceCurrentUrl(params)
+  }
+
+  startAuth() {
+    const form = document.createElement("form")
+    form.method = "post"
+    form.action = this.authPathValue
+    form.style.display = "none"
+
+    this.appendHiddenField(form, "authenticity_token", this.csrfToken())
+    this.appendHiddenField(form, "origin", this.originValue || window.location.pathname)
+
+    document.body.appendChild(form)
+    form.submit()
+  }
+
+  private csrfToken() {
+    const token = document.querySelector<HTMLMetaElement>("meta[name='csrf-token']")?.content
+    return token || ""
+  }
+
+  private appendHiddenField(form: HTMLFormElement, name: string, value: string) {
+    const input = document.createElement("input")
+    input.type = "hidden"
+    input.name = name
+    input.value = value
+    form.appendChild(input)
   }
 
   private replaceCurrentUrl(params: URLSearchParams) {
