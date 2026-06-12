3d8083c96a708ab8b887621a0e2b7e354e239cbf
Scott F <Scott.Fisher@ucop.edu>
2026-06-12 16:01:23 -0700

    Initial bunch of changes to implement orcid login/callback.

---
+++ spec/requests/unauthenticated/omniauth_callbacks_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe Unauthenticated::OmniauthCallbacksController, type: :request do
+  describe "GET /users/auth/orcid/callback" do
+    around do |example|
+      previous_auth = Rails.application.env_config["omniauth.auth"]
+      previous_origin = Rails.application.env_config["omniauth.origin"]
+
+      example.run
+    ensure
+      Rails.application.env_config["omniauth.auth"] = previous_auth
+      Rails.application.env_config["omniauth.origin"] = previous_origin
+    end
+
+    it "redirects back to origin with ORCID in query params" do
+      Rails.application.env_config["omniauth.auth"] = OmniAuth::AuthHash.new(uid: "0000-0002-1825-0097")
+      Rails.application.env_config["omniauth.origin"] = "/users/sign_up?source=registration"
+
+      get "/users/auth/orcid/callback"
+
+      expect(response).to redirect_to("/users/sign_up?source=registration&orcid=0000-0002-1825-0097&orcid_callback=1")
+    end
+
+    it "falls back to sign-up path for unsafe origin" do
+      Rails.application.env_config["omniauth.auth"] = OmniAuth::AuthHash.new(uid: "0000-0002-1825-0097")
+      Rails.application.env_config["omniauth.origin"] = "https://malicious.example/redirect"
+
+      get "/users/auth/orcid/callback"
+
+      expect(response).to redirect_to("/users/sign_up?orcid=0000-0002-1825-0097&orcid_callback=1")
+    end
+  end
+end
