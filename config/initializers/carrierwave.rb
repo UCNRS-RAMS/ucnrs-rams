da02d5a3974e09704d8da28f50e2c4a21889a4ad
Scott F <Scott.Fisher@ucop.edu>
2026-07-01 10:42:17 -0700

    I believe this should work with AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,17 +16,30 @@ if Rails.env.test?
   end
 end
 
-if Rails.env.production?
+if %w[dev_server staging production].include?(Rails.env)
   CarrierWave.configure do |config|
     config.storage = :fog
     config.root = Rails.public_path.to_s.freeze
-    config.fog_credentials = {
+
+    fog_creds = {
       provider: "AWS",
-      aws_access_key_id: ENV.fetch("AWS_ACCESS_KEY", nil),
-      aws_secret_access_key: ENV.fetch("AWS_SECRET_KEY", nil),
-      region: ENV.fetch("AWS_REGION", nil),
+      region: ENV.fetch("AWS_REGION", "us-west-2") # Fallback to default region
     }
+
+    # Only manually inject keys if they are explicitly provided in the environment for an external server.
+    # If running within the same AWS Account for ECS and S3 then access can be provided with policies and
+    # CloudFormation rather than embedding external AWS access values.
+    if ENV["AWS_ACCESS_KEY"].present? && ENV["AWS_SECRET_KEY"].present?
+      fog_creds[:aws_access_key_id] = ENV["AWS_ACCESS_KEY"]
+      fog_creds[:aws_secret_access_key] = ENV["AWS_SECRET_KEY"]
+    else
+      # This triggers fog/AWS SDK to use the ECS Task Role automatically
+      fog_creds[:use_iam_profile] = true
+    end
+
+    config.fog_credentials = fog_creds
     config.fog_directory = ENV.fetch("BUCKET_NAME", nil)
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
+
 end
