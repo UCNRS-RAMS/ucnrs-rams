83ff2f897e059267a6c6af08d323d9099502e206
Scott F <Scott.Fisher@ucop.edu>
2026-08-05 14:38:59 -0700

    Only changes from authenticated to unauthenticated in manager view if the manager opts to change it for the user instead of leaving it alone.

--- app/javascript/controllers/toggle_controller.ts
+++ app/javascript/controllers/toggle_controller.ts
@@ -1,9 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["toggle"]
+  static targets = ["toggle", "authenticated"]
 
   declare toggleTargets: HTMLElement[]
+  declare authenticatedTarget: HTMLInputElement
+  declare hasAuthenticatedTarget: boolean
 
   toggle(e: Event) {
     e.preventDefault()
@@ -11,6 +13,12 @@ export default class extends Controller {
     this.toggleTargets.forEach(target => {
       this.toggleCSSClass(target)
     })
+  }
+
+  markUnauthenticated() {
+    if (this.hasAuthenticatedTarget) {
+      this.authenticatedTarget.value = "false"
+    }
   }
 
   toggleCSSClass(target) {
