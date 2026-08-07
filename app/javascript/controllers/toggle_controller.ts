ddee158d3283e08f47dafa35c98404bf57abf9cd
Scott F <Scott.Fisher@ucop.edu>
2026-08-07 10:07:59 -0700

    Changes functionality to disallow managers changing authenticated orcids

--- app/javascript/controllers/toggle_controller.ts
+++ app/javascript/controllers/toggle_controller.ts
@@ -1,11 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["toggle", "authenticated"]
+  static targets = ["toggle"]
 
   declare toggleTargets: HTMLElement[]
-  declare authenticatedTarget: HTMLInputElement
-  declare hasAuthenticatedTarget: boolean
 
   toggle(e: Event) {
     e.preventDefault()
@@ -13,12 +11,6 @@ export default class extends Controller {
     this.toggleTargets.forEach(target => {
       this.toggleCSSClass(target)
     })
-  }
-
-  markUnauthenticated() {
-    if (this.hasAuthenticatedTarget) {
-      this.authenticatedTarget.value = "false"
-    }
   }
 
   toggleCSSClass(target) {
