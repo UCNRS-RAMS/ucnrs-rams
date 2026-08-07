51fa0a828640676f37c61819620496b18b2e50e8
Scott F <Scott.Fisher@ucop.edu>
2026-08-07 10:07:59 -0700

    Changes functionality to disallow managers changing authenticated orcids

--- app/javascript/__tests__/toggle_controller_spec.ts
+++ app/javascript/__tests__/toggle_controller_spec.ts
@@ -16,21 +16,18 @@ describe("ToggleController", () => {
         <section data-controller="toggle">
           <input id="input_one" data-toggle-target="toggle" data-toggle-class="blink" class="one blink"/>
           <input id="input_two" data-toggle-target="toggle" data-toggle-class="blink" class="two"/>
-          <input id="authenticated" data-toggle-target="authenticated" value="true"/>
-          <a id="one" data-action="click->toggle#toggle click->toggle#markUnauthenticated">Toggle!</a>
+          <a id="one" data-action="click->toggle#toggle">Toggle!</a>
         </section>`)
     })
 
     it("toggles the presenceof the data-toggle-class name in classList", () => {
       const inputOne = document.getElementById("input_one")
       const inputTwo = document.getElementById("input_two")
-      const authenticated = document.getElementById("authenticated") as HTMLInputElement
       const link = document.getElementById("one")
 
       link.click()
       expect(inputOne.className).toEqual("one")
       expect(inputTwo.className).toEqual("two blink")
-      expect(authenticated.value).toEqual("false")
 
       link.click()
       expect(inputOne.className).toEqual("one blink")
