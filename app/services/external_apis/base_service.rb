6579d8c2b71b586c502b07d5ffe7ea90bab06db7
Scott F <Scott.Fisher@ucop.edu>
2026-08-12 10:05:25 -0700

    first step in refactoring for ror

---
+++ app/services/external_apis/base_service.rb
@@ -0,0 +1,219 @@
+# from path app/services/external_apis/base_service.rb
+
+# frozen_string_literal: true
+
+require 'httparty'
+require 'digest'
+require 'zip'
+
+module ExternalApis
+  # Errors for External Api services
+  class ExternalApiError < StandardError; end
+
+  # Abstract service that provides HTTP methods for individual external api services
+  class BaseService
+    class << self
+      # The following should be defined in each inheriting service's initializer.
+      # For example:
+      #   ExternalApis::RorService.setup do |config|
+      #     config.x.ror.landing_page_url = "https://example.org/"
+      #     config.x.ror.api_base_url = "https://api.example.org/"
+      #   end
+      def landing_page_url
+        nil
+      end
+
+      def api_base_url
+        nil
+      end
+
+      def max_pages
+        5
+      end
+
+      def max_results_per_page
+        50
+      end
+
+      def max_redirects
+        3
+      end
+
+      def active?
+        false
+      end
+
+      # The standard headers to be used when communicating with an external API.
+      # These headers can be overriden or added to when calling an external API
+      # by sending your changes in the `additional_headers` attribute of
+      # `http_get`
+      def headers
+        hash = {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'User-Agent': "#{app_name} (#{app_email})"
+        }
+        hash.merge({ Host: URI(api_base_url).hostname.to_s })
+      rescue URI::InvalidURIError => e
+        handle_uri_failure(method: "BaseService.headers #{e.message}",
+                           uri: api_base_url)
+        hash
+      end
+
+      # Logs the results of a failed HTTP response
+      def handle_http_failure(method:, http_response:)
+        content = http_response.inspect
+        msg = "received a #{http_response&.code} response with: #{content}!"
+        log_error(method: method, error: ExternalApiError.new(msg))
+      end
+
+      # Logs the results of a failed HTTP response
+      def handle_uri_failure(method:, uri:)
+        msg = "received an invalid uri: '#{uri&.to_s}'!"
+        log_error(method: method, error: ExternalApiError.new(msg))
+      end
+
+      # Logs the specified error along with the full backtrace
+      def log_error(method:, error:)
+        return unless method.present? && error.present?
+
+        Rails.logger.error "#{self.class.name}.#{method} #{error.message}"
+        Rails.logger.error error.backtrace
+      end
+
+      # Logs the specified message (as INFO by default, WARN otherwise)
+      def log_message(method:, message:, info: true)
+        return unless method.present? && message.present?
+
+        Rails.logger.send((info ? :info : :warn), "#{self.class.name}.#{method} #{message}")
+      end
+
+      # Emails the error and response to the administrators
+      # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      def notify_administrators(obj:, response: nil, error: nil)
+        return false unless obj.present? && response.present?
+
+        message = "#{obj.class.name} - #{obj.respond_to?(:id) ? obj.id : ''}"
+        message += '<br>----------------------------------------<br><br>'
+
+        message += "Sent: #{Rails.logger.debug(json_from_template(plan: obj))}" if obj.is_a?(Plan)
+        message += '<br>----------------------------------------<br><br>' if obj.is_a?(Plan)
+
+        message += "#{name} received the following unexpected response:<br>"
+        message += response.inspect.to_s
+        message += '<br>----------------------------------------<br><br>'
+
+        message += error.message if error.present? && error.is_a?(StandardError)
+        message += error.backtrace || '' if error.present? && error.is_a?(StandardError)
+
+        UserMailer.notify_administrators(message).deliver_now
+        true
+      end
+      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+      private
+
+      # Retrieves the application name from dmproadmap.rb initializer or uses the App name
+      def app_name
+        ApplicationService.application_name
+      end
+
+      # Retrieves the helpdesk email from dmproadmap.rb initializer or uses the contact page url
+      def app_email
+        dflt = Rails.application.routes.url_helpers.contact_us_url || ''
+        Rails.configuration.x.organisation.fetch(:helpdesk_email, dflt)
+      end
+
+      # Makes a GET request to the specified uri with the additional headers.
+      # Additional headers are combined with the base headers defined above.
+      def http_get(uri:, additional_headers: {}, debug: false)
+        return nil if uri.blank?
+
+        HTTParty.get(uri, options(additional_headers: additional_headers,
+                                  debug: debug))
+      rescue URI::InvalidURIError => e
+        handle_uri_failure(method: "BaseService.http_get #{e.message}",
+                           uri: uri)
+        nil
+      rescue HTTParty::Error => e
+        handle_http_failure(method: "BaseService.http_get #{e.message}",
+                            http_response: nil)
+        nil
+      end
+
+      # Makes a PUT request to the specified uri with the additional headers.
+      # Additional headers are combined with the base headers defined above.
+      def http_put(uri:, additional_headers: {}, data: {}, basic_auth: nil, debug: false)
+        return nil if uri.blank?
+
+        opts = options(additional_headers: additional_headers, debug: debug)
+        opts[:body] = data
+        opts[:basic_auth] = basic_auth if basic_auth.present?
+        HTTParty.put(uri, opts)
+      rescue URI::InvalidURIError => e
+        handle_uri_failure(method: "BaseService.http_put #{e.message}", uri: uri)
+        nil
+      rescue HTTParty::Error => e
+        handle_http_failure(method: "BaseService.http_put #{e.message}", http_response: nil)
+        nil
+      end
+
+      # Makes a POST request to the specified uri with the additional headers.
+      # Additional headers are combined with the base headers defined above.
+      def http_post(uri:, additional_headers: {}, data: {}, basic_auth: nil, debug: false)
+        return nil if uri.blank?
+
+        opts = options(additional_headers: additional_headers, debug: debug)
+        opts[:body] = data
+        opts[:basic_auth] = basic_auth if basic_auth.present?
+        HTTParty.post(uri, opts)
+      rescue URI::InvalidURIError => e
+        handle_uri_failure(method: "BaseService.http_post #{e.message}", uri: uri)
+        nil
+      rescue HTTParty::Error => e
+        handle_http_failure(method: "BaseService.http_post #{e.message}", http_response: nil)
+        nil
+      end
+
+      # Options for the HTTParty call
+      def options(additional_headers: {}, debug: false)
+        hash = {
+          headers: headers.merge(additional_headers),
+          follow_redirects: true,
+          limit: 6
+        }
+        hash[:debug_output] = $stdout if debug
+        hash
+      end
+
+      # Unzips the specified file
+      def unzip_file(zip_file:, destination:)
+        return false unless zip_file.present? && File.exist?(zip_file)
+
+        Zip::File.open(zip_file) do |files|
+          files.each do |entry|
+            next if File.exist?(entry.name)
+
+            f_path = File.join(destination, entry.name)
+            FileUtils.mkdir_p(File.dirname(f_path))
+            files.extract(entry, f_path) unless File.exist?(f_path)
+          end
+        end
+        true
+      end
+
+      # Determine if the downloaded file matches the expected checksum
+      def validate_downloaded_file(file_path:, checksum:)
+        return false unless file_path.present? && checksum.present? && File.exist?(file_path)
+
+        possible_checksums = [
+          Digest::SHA1.file(file_path).to_s,
+          Digest::SHA256.file(file_path).to_s,
+          Digest::SHA512.file(file_path).to_s,
+          Digest::MD5.file(file_path).to_s
+        ]
+        possible_checksums.include?(checksum)
+      end
+    end
+  end
+end
---
+++ app/services/external_apis/ror_service.rb
@@ -0,0 +1,306 @@
+# app/services/external_apis/ror_service.rb
+
+# frozen_string_literal: true
+
+require 'digest'
+
+module ExternalApis
+  # This service provides an interface to the Research Organization Registry (ROR)
+  # API.
+  # For more information: https://github.com/ror-community/ror-api
+  class RorService < BaseService
+    class << self
+      # Retrieve the config settings from the initializer
+      def landing_page_url
+        Rails.configuration.x.ror&.landing_page_url || super
+      end
+
+      def api_base_url
+        Rails.configuration.x.ror&.api_base_url || super
+      end
+
+      def download_url
+        Rails.configuration.x.ror&.download_url
+      end
+
+      def full_catalog_file
+        Rails.configuration.x.ror&.full_catalog_file
+      end
+
+      def file_dir
+        Rails.configuration.x.ror&.file_dir
+      end
+
+      def checksum_file
+        Rails.configuration.x.ror&.checksum_file
+      end
+
+      def zip_file
+        Rails.configuration.x.ror&.zip_file
+      end
+
+      def active?
+        Rails.configuration.x.ror&.active || super
+      end
+
+      def heartbeat_path
+        Rails.configuration.x.ror&.heartbeat_path
+      end
+
+      def search_path
+        Rails.configuration.x.ror&.search_path
+      end
+
+      # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      def fetch(force: false)
+        method = "ExternalApis::RorService.fetch(force: #{force})"
+
+        # Fetch the Zenodo metadata for ROR to see if we have the latest data dump
+        metadata = fetch_zenodo_metadata
+
+        if metadata.present?
+          FileUtils.mkdir_p(file_dir)
+
+          checksum = File.open(checksum_file, 'w+') unless File.exist?(checksum_file) && !force
+          checksum = File.open(checksum_file, 'r+') if checksum.blank?
+          old_checksum_val = checksum.read
+
+          if old_checksum_val == metadata[:checksum]
+            log_message(method: method, message: 'There is no new ROR file to process.')
+          else
+            download_file = metadata.fetch(:links, {})[:download]
+            log_message(method: method, message: "New ROR file detected - checksum #{metadata[:checksum]}")
+            log_message(method: method, message: "Downloading #{download_file}")
+
+            payload = download_ror_file(url: metadata.fetch(:links, {})[:download])
+            if payload.present?
+              file = File.open(zip_file, 'wb')
+              file.write(payload)
+
+              # rubocop:disable Metrics/BlockNesting
+              if validate_downloaded_file(file_path: zip_file, checksum: metadata[:checksum])
+                json_file = download_file.split('/').last.gsub('.zip', '')
+                json_file = "#{json_file}.json" unless json_file.end_with?('.json')
+
+                # Process the ROR JSON
+                if process_ror_file(zip_file: zip_file, file: json_file)
+                  checksum = File.open(checksum_file, 'w')
+                  checksum.write(metadata[:checksum])
+                end
+              else
+                log_error(method: method, error: StandardError.new('Downloaded ROR zip does not match checksum!'))
+              end
+              # rubocop:enable Metrics/BlockNesting
+            else
+              log_error(method: method, error: StandardError.new('Unable to download ROR file!'))
+            end
+          end
+        else
+          log_error(method: method, error: StandardError.new('Unable to fetch ROR metadata from Zenodo!'))
+        end
+      end
+      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+      # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+      private
+
+      # Fetch the latest Zenodo metadata for ROR files
+      # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      def fetch_zenodo_metadata
+        Rails.logger.error 'No :download_url defined for RorService!' if download_url.blank?
+        return nil if download_url.blank?
+
+        # Fetch the latest ROR metadata from Zenodo (the query will place the most recent
+        # version 1st)
+        resp = http_get(uri: download_url, additional_headers: { host: 'zenodo.org' }, debug: false)
+        unless resp.present? && resp.code == 200
+          handle_http_failure(method: 'Fetching ROR metadata from Zenodo', http_response: resp)
+          notify_administrators(obj: 'RorService', response: resp)
+          return nil
+        end
+        json = JSON.parse(resp.body)
+
+        # Extract the most recent file's metadata
+        file_metadata = json.first.fetch('files', []).first&.with_indifferent_access
+        unless file_metadata.present? && file_metadata.fetch(:links, {})[:download].present?
+          handle_http_failure(method: 'No file found in ROR metadata from Zenodo', http_response: resp)
+          notify_administrators(obj: 'RorService', response: resp)
+          return nil
+        end
+
+        file_metadata
+      rescue JSON::ParserError => e
+        log_error(method: 'RorService', error: e)
+        nil
+      end
+      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+      # Download the latest ROR data
+      def download_ror_file(url:)
+        return nil if url.blank?
+
+        headers = {
+          host: 'zenodo.org',
+          Accept: 'application/zip'
+        }
+        resp = http_get(uri: url, additional_headers: headers, debug: false)
+        unless resp.present? && resp.code == 200
+          handle_http_failure(method: "Fetching ROR file from Zenodo - #{url}", http_response: resp)
+          notify_administrators(obj: 'RorService', response: resp)
+          return nil
+        end
+        resp.body
+      end
+
+      # Parse the JSON file and process each individual record
+      # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      def process_ror_file(zip_file:, file:)
+        return false unless zip_file.present? && file.present?
+
+        if unzip_file(zip_file: zip_file, destination: file_dir)
+          method = 'ExternalApis::RorService.process_ror-file'
+          if File.exist?("#{file_dir}/#{file}")
+            json_file = File.open("#{file_dir}/#{file}", 'r')
+            json = JSON.parse(json_file.read)
+            cntr = 0
+            total = json.length
+            json.each do |hash|
+              cntr += 1
+              log_message(method: method, message: "Processed #{cntr} out of #{total} records") if (cntr % 1000).zero?
+
+              hash = hash.with_indifferent_access if hash.is_a?(Hash)
+
+              next if process_ror_record(record: hash, time: json_file.mtime)
+
+              log_message(
+                method: method,
+                message: "Unable to process record for: '#{hash&.fetch('name', 'unknown')}'",
+                info: false
+              )
+            end
+            # Remove any old ROR records (their file_timestamps would not have been updated)
+            # Note this does not remove any associated Org records!
+            RorOrg.where('file_timestamp < ?', json_file.mtime.strftime('%Y-%m-%d %H:%M:%S')).destroy_all
+            true
+          else
+            log_error(method: method, error: StandardError.new('Unable to find json in zip!'))
+            false
+          end
+        else
+          log_error(method: method, error: StandardError.new('Unable to unzip contents of ROR file'))
+          false
+        end
+      rescue JSON::ParserError => e
+        log_error(method: method, error: e)
+        false
+      end
+      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+      # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+      # Transfer the contents of the JSON record to the org_indices table
+      # rubocop:disable Metrics/AbcSize
+      def process_ror_record(record:, time:)
+        return nil unless record.present? && record.is_a?(Hash) && record['id'].present?
+
+        ror_org = RorOrg.find_or_create_by(ror_id: record['id'])
+        ror_org.name = safe_string(value: org_name(item: record))
+        ror_org.acronyms = record['acronyms']
+        ror_org.aliases = record['aliases']
+        ror_org.country = record['country']
+        ror_org.types = record['types']
+        ror_org.language = org_language(item: record)
+        ror_org.file_timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+        ror_org.fundref_id = fundref_id(item: record)
+        ror_org.home_page = safe_string(value: record.fetch('links', []).first)
+
+        # Attempt to find a matching Org record
+        ror_org.org_id = check_for_org_association(ror_org: ror_org)
+
+        # TODO: We should create some sort of Super Admin page to highlight unmapped
+        #       RorOrg records so that they can be connected to their Org
+        ror_org.save
+        true
+      rescue StandardError => e
+        log_error(method: 'ExternalApis::RorService.process_ror_record', error: e)
+        log_message(method: 'ExternalApis::RorService.process_ror-record', message: record.to_s)
+        false
+      end
+      # rubocop:enable Metrics/AbcSize
+
+      def safe_string(value:)
+        return value if value.blank? || value.length < 255
+
+        value[0..254]
+      end
+
+      # Determine if there is a matching Org record in the DB if so, attach it
+      def check_for_org_association(ror_org:)
+        return ror_org.org&.id if ror_org.org.present?
+
+        ror = Identifier.by_scheme_name('ror', 'Org')
+                        .where(value: ror_org.ror_id)
+                        .first
+        return nil if ror.blank?
+
+        ror.present? ? ror.identifiable_id : nil
+      end
+
+      # Org names are not unique, so include the Org URL if available or
+      # the country. For example:
+      #    "Example College (example.edu)"
+      #    "Example College (Brazil)"
+      def org_name(item:)
+        return '' unless item.present? && item['name'].present?
+
+        country = item.fetch('country', {}).fetch('country_name', '')
+        website = org_website(item: item)
+        # If no website or country then just return the name
+        return item['name'] unless website.present? || country.present?
+
+        # Otherwise return the contextualized name
+        "#{item['name']} (#{website || country})"
+      end
+
+      # Extracts the org's ISO639 if available
+      def org_language(item:)
+        dflt = I18n.default_locale || 'en'
+        return dflt if item.blank?
+
+        country = item.fetch('country', {}).fetch('country_code', '')
+        labels = case country
+                 when 'US'
+                   [{ iso639: 'en' }]
+                 else
+                   item.fetch('labels', [{ iso639: dflt }])
+                 end
+        labels.first&.fetch('iso639', I18n.default_locale) || dflt
+      end
+
+      # Extracts the website domain from the item
+      def org_website(item:)
+        return nil unless item.present? && item.fetch('links', [])&.any?
+        return nil if item['links'].first.blank?
+
+        # A website was found, so extract just the domain without the www
+        domain_regex = %r{^(?:http://|www\.|https://)([^/]+)}
+        website = item['links'].first.scan(domain_regex).last.first
+        website.gsub('www.', '')
+      end
+
+      # Extracts the FundRef Id if available
+      def fundref_id(item:)
+        return '' unless item.present? && item['external_ids'].present?
+        return '' unless item['external_ids'].fetch('FundRef', {}).any?
+
+        # If a preferred Id was specified then use it
+        ret = item['external_ids'].fetch('FundRef', {}).fetch('preferred', '')
+        return ret if ret.present?
+
+        # Otherwise take the first one listed
+        item['external_ids'].fetch('FundRef', {}).fetch('all', []).first
+      end
+    end
+  end
+end
