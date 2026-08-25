5e45947a8bfc6958546a553435c02967cd0c3a94
Manuel M <manuelm@ucr.edu>
2026-08-25 12:20:28 -0700

    Fix Turbo compatibility for Devise
    When I first switched to Rails Turbo, the Devise gem were not Turbo-compatible, causing rejected form submissions and redirect issues. Therefore it was turned off for all forms related to Devise.
    
    Current version of Devise do now support Turbo. This change removes all the element flag `data: { turbo: false }` to reenable Turbo, adds the standard Devise responder status configuration, and fix the redirect to handle the enabled Devise Turbo login.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,6 +254,10 @@ Devise.setup do |config|
   # only the current scope. By default, Devise signs out all scopes.
   # config.sign_out_all_scopes = true
 
+  # ==> Hotwire/Turbo configuration
+  config.responder.error_status = :unprocessable_content
+  config.responder.redirect_status = :see_other
+
   # ==> Navigation configuration
   # Lists the formats that should be treated as navigational. Formats like
   # :html, should redirect to the sign in page when the user does not have
