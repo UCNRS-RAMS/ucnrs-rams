61596b133862c471287c6fb850c0aa91f552d468
Scott F <Scott.Fisher@ucop.edu>
2026-09-21 11:51:49 -0700

    Updating tests.

--- spec/services/imports/ror_association_spec.rb
+++ spec/services/imports/ror_association_spec.rb
@@ -3,17 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Imports::RorAssociation do
-  describe '#initialize' do
-    it 'stores the csv path' do
-      importer = described_class.new('/tmp/ror_association.csv')
-
-      expect(importer.instance_variable_get(:@csv_path)).to eq('/tmp/ror_association.csv')
-    end
-  end
-
-  describe '#call' do
+  describe '.process_spreadsheet' do
     it 'raises an error when no csv path is supplied' do
-      expect { described_class.new.call }.to raise_error(ArgumentError, 'CSV path is required')
+      expect { described_class.process_spreadsheet(nil) }.to raise_error(ArgumentError, 'CSV path is required')
     end
 
     it 'updates matching institutions and returns them' do
@@ -22,8 +14,7 @@ RSpec.describe Imports::RorAssociation do
       csv_file.write("rams_id,rams_name,rams_city,ror_id\n#{institution.id},university of california,oakland,https://ror.org/123\n")
       csv_file.rewind
 
-      importer = described_class.new(csv_file.path)
-      expect(importer.call).to contain_exactly(institution)
+      expect(described_class.process_spreadsheet(csv_file.path)).to contain_exactly(institution)
       expect(institution.reload.ror_id).to eq('https://ror.org/123')
     ensure
       csv_file.close
@@ -33,7 +24,7 @@ RSpec.describe Imports::RorAssociation do
 
   describe '.normalize_for_match' do
     it 'strips surrounding whitespace and lowercases the value' do
-      expect(described_class.normalize_for_match('  Example Institute  ')).to eq('example institute')
+      expect(described_class.send(:normalize_for_match, '  Example Institute  ')).to eq('example institute')
     end
   end
 
@@ -41,7 +32,7 @@ RSpec.describe Imports::RorAssociation do
     it 'normalizes all values and keeps indifferent access' do
       row = { 'rams_name' => '  University of California  ', 'rams_city' => ' Oakland ' }
 
-      normalized = described_class.normalize_row(row)
+      normalized = described_class.send(:normalize_row, row)
 
       expect(normalized[:rams_name]).to eq('university of california')
       expect(normalized['rams_city']).to eq('oakland')
@@ -58,7 +49,7 @@ RSpec.describe Imports::RorAssociation do
         ror_id: 'https://ror.org/123'
       }
 
-      expect(described_class.find_matching_institutions(row)).to contain_exactly(institution)
+      expect(described_class.send(:find_matching_institutions, row)).to contain_exactly(institution)
     end
 
     it 'falls back to name and city when the id is stale or belongs to a different institution' do
@@ -70,7 +61,7 @@ RSpec.describe Imports::RorAssociation do
         ror_id: 'https://ror.org/123'
       }
 
-      expect(described_class.find_matching_institutions(row)).to contain_exactly(institution)
+      expect(described_class.send(:find_matching_institutions, row)).to contain_exactly(institution)
     end
 
     it 'returns an empty array when the row does not include an ror id' do
@@ -82,13 +73,13 @@ RSpec.describe Imports::RorAssociation do
         ror_id: ''
       }
 
-      expect(described_class.find_matching_institutions(row)).to eq([])
+      expect(described_class.send(:find_matching_institutions, row)).to eq([])
     end
   end
 
   describe '.matching_string?' do
     it 'ignores case and surrounding whitespace' do
-      expect(described_class.matching_string?('  Example Institute  ', 'example institute')).to be(true)
+      expect(described_class.send(:matching_string?, '  Example Institute  ', 'example institute')).to be(true)
     end
   end
 end
